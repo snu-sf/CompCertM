ef4a418832758bf6077742d1d3572e23a0f67801
alxest <youngju.song@sf.snu.ac.kr>
2018-07-03 12:36:19 +0900

    Fix MatchSimModSem to compile

--- proof/MatchSimModSem.v
+++ proof/MatchSimModSem.v
@@ -1,12 +1,13 @@
 Require Import CoqlibC.
 Require Import SmallstepC.
+Require Import Simulation.
 Require Import ModSem AsmregsC GlobalenvsC MemoryC ASTC.
 Require Import SimModSem SimMem SimSymb.
 
 Set Implicit Arguments.
 
 
-Section MATCHSIMMODSEM.
+Section MATCHSIMFORWARD.
 
   Context {SM: SimMem.class} {SS: SimSymb.class SM}.
 
@@ -64,7 +65,7 @@ Section MATCHSIMMODSEM.
         /\
         (<<MCOMPAT: mem_compat st_init_src st_init_tgt sm_init>>)
         /\
-        (<<MATCH: match_states rs_arg_src rs_arg_tgt sm_init
+        (<<MATCH: match_states rs_arg_src rs_arg_tgt sm_arg
                                idx_init st_init_src st_init_tgt sm_init>>)
   .
 
@@ -76,6 +77,8 @@ Section MATCHSIMMODSEM.
       (CALLSRC: ms_src.(ModSem.is_call) st_src0)
     ,
       (<<CALLTGT: ms_tgt.(ModSem.is_call) st_tgt0>>)
+      /\
+      (<<MWF: SimMem.wf sm0>>)
   .
 
   Hypothesis ATBSIM: forall
@@ -137,6 +140,29 @@ Section MATCHSIMMODSEM.
         (<<FINALTGT: ms_tgt.(ModSem.final_frame) rs_init_tgt st_tgt0 rs_ret_tgt>>)
         /\
         (<<RSREL: SimMem.sim_regset sm0 rs_ret_src rs_ret_tgt>>)
+        /\
+        (<<MWF: SimMem.wf sm0>>)
+  .
+
+  Hypothesis STEPFSIM: forall
+      rs_init_src rs_init_tgt sm_init idx0 st_src0 st_tgt0 sm0
+      (MATCH: match_states rs_init_src rs_init_tgt sm_init
+                           idx0 st_src0 st_tgt0 sm0)
+    ,
+      (<<RECEP: receptive_at ms_src st_src0>>)
+      /\
+      (<<STEPFSIM: forall
+             tr st_src1
+             (STEPSRC: Step ms_src st_src0 tr st_src1)
+           ,
+             exists idx1 st_tgt1 sm1,
+               (<<PLUS: DPlus ms_tgt st_tgt0 tr st_tgt1>> \/
+                           <<STAR: DStar ms_tgt st_tgt0 tr st_tgt1 /\ order idx1 idx0>>)
+               /\ (<<MCOMPAT: mem_compat st_src1 st_tgt1 sm1>>) (* TODO: <-------- is this needed??????? *)
+               /\ (<<MLE: SimMem.le sm0 sm1>>)
+               (* Note: We require le for mle_preserves_sim_ge, but we cannot require SimMem.wf, beacuse of DCEproof *)
+               /\ (<<MATCH: match_states rs_init_src rs_init_tgt sm_init idx1 st_src1 st_tgt1 sm1>>)
+                    >>)
   .
 
   Hypothesis BAR: bar_True.
@@ -162,14 +188,14 @@ Section MATCHSIMMODSEM.
                                                                 sm_arg.(SimMem.src_mem) st_init_src)
         ,
           exists st_init_tgt sm_init idx_init,
-            (* (<<MCOMPAT: mem_compat ms_src ms_tgt st_init_src st_init_tgt sm_init>>) /\ *)
-            (* Can be proved with initial_states_get_mem *)
+            (<<MCOMPAT: mem_compat st_init_src st_init_tgt sm_init>>)
+            /\
             (<<INITTGT: msp.(ModSemPair.tgt).(ModSem.initial_frame) rs_init_tgt
                                                                     sm_arg.(SimMem.tgt_mem) st_init_tgt>>)
             /\
             (<<MLE: SimMem.le sm_arg sm_init>>)
             /\
-            (<<SIM: match_states rs_init_src rs_init_tgt sm_init
+            (<<SIM: match_states rs_init_src rs_init_tgt sm_arg
                                  idx_init st_init_src st_init_tgt sm_init>>)>>)
   .
   Proof.
@@ -186,10 +212,20 @@ Section MATCHSIMMODSEM.
 
   Hint Unfold to_idx.
 
+  Lemma to_idx_spec
+        i0 i1
+        (ORD: order i0 i1)
+    :
+      <<ORD: Ord.ord i0.(to_idx) i1.(to_idx)>>
+  .
+  Proof.
+    econs; eauto. cbn. instantiate (1:= eq_refl). cbn. ss.
+  Qed.
+
   Lemma match_states_lxsim
         rs_init_src rs_init_tgt sm_init i0 st_src0 st_tgt0 sm0
         (MLE: SimMem.le sm_init sm0)
-        (MWF: SimMem.wf sm0)
+        (* (MWF: SimMem.wf sm0) *)
         (MCOMPAT: mem_compat st_src0 st_tgt0 sm0)
         (MATCH: match_states rs_init_src rs_init_tgt sm_init i0 st_src0 st_tgt0 sm0)
     :
@@ -200,11 +236,12 @@ Section MATCHSIMMODSEM.
     pcofix CIH. i. pfold.
     generalize (classic (ModSem.is_call ms_src st_src0)). intro CALLSRC; des.
     {
+      exploit ATPROGRESS; eauto. i; des.
       eapply lxsim_at_external; eauto.
-      {
-        u in CALLSRC. des. rename rs_arg into rs_arg_src. rename m_arg into m_arg_src.
-        exploit ATPROGRESS; eauto.
-      }
+      (* { *)
+      (*   u in CALLSRC. des. rename rs_arg into rs_arg_src. rename m_arg into m_arg_src. *)
+      (*   exploit ATPROGRESS; eauto. *)
+      (* } *)
       clear CALLSRC.
       i.
       exploit ATBSIM; eauto. i; des.
@@ -223,7 +260,6 @@ Section MATCHSIMMODSEM.
           - eapply SimMem.unlift_spec; eauto.
           - etransitivity; eauto.
         }
-        { eapply SimMem.unlift_wf; eauto. }
         { econs.
           - erewrite ModSem.after_external_get_mem; try eassumption. erewrite SimMem.unlift_src; eauto.
           - erewrite ModSem.after_external_get_mem; try eassumption. erewrite SimMem.unlift_tgt; eauto.
@@ -237,9 +273,15 @@ Section MATCHSIMMODSEM.
     }
     {
       eapply lxsim_step_forward; eauto.
-      { econs 1. (* TODO: SIMPLIFY THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! *)
-        ii.
-      }
+      exploit STEPFSIM; eauto. i; des.
+      econs 1; eauto.
+      ii. exploit STEPFSIM0; eauto. i; des_safe.
+      esplits; eauto.
+      - des.
+        + left. eauto.
+        + right. esplits; eauto. eapply to_idx_spec; eauto.
+      - right. eapply CIH; eauto.
+        { etransitivity; eauto. }
     }
   Qed.
 
@@ -254,14 +296,31 @@ Section MATCHSIMMODSEM.
     exploit SimSymb.sim_skenv_func_bisim; eauto. intro FSIM; des.
     Print SimSymb.sim_skenv.
     inv FSIM. exploit FUNCFSIM; eauto. i; des.
-    split.
-    - i.
-    exploit INITMATCHFORWARD; eauto.
-    {
+    exploit init_match_states; eauto. i; des.
     esplits; eauto.
-    - econs; eauto.
-      exploit INITMATCH; eauto.
+    eapply match_states_lxsim; eauto.
   Qed.
 
-End MATCHSIMMODSEM.
+End MATCHSIMFORWARD.
 
+
+      (* (<<BSIM: *)
+      (*    (<<BSIMSTEP: forall *)
+      (*        tr st_tgt1 *)
+      (*        (STEPTGT: Step ms_tgt st_tgt0 tr st_tgt1) *)
+      (*      , *)
+      (*        exists idx1 st_src1 sm1, *)
+      (*          (<<PLUS: Plus ms_src st_src0 tr st_src1>> \/ *)
+      (*                   <<STAR: Star ms_src st_src0 tr st_src1 /\ order idx1 idx0>>) *)
+      (*          /\ (<<MCOMPAT: mem_compat st_src1 st_tgt1 sm1>>) (* TODO: <-------- is this needed??????? *) *)
+      (*          /\ (<<MLE: SimMem.le sm0 sm1>>) *)
+      (*          (* Note: We require le for mle_preserves_sim_ge, but we cannot require SimMem.wf, beacuse of DCEproof *) *)
+      (*          /\ (<<MATCH: match_states rs_init_src rs_init_tgt sm_init idx1 st_src1 st_tgt1 sm1>>) *)
+      (*               >>) *)
+      (*    /\ *)
+      (*    (<<PROGRESS: forall *)
+      (*        (SAFESRC: safe ms_src st_src0) *)
+      (*      , *)
+      (*        <<FINAL: exists retv, final_state ms_tgt st_tgt0 retv>> \/ *)
+      (*        <<STEPTGT: exists tr st_tgt1, Step ms_tgt st_tgt0 tr st_tgt1>>>>) *)
+      (*    >>) *)

95ed5b54e53a6ca1617ed699f5a4de532e03f8d6
Dongjoo-Kim <kimdj316@naver.com>
2019-04-11 16:28:08 +0900

    WIP

--- compose/SimSymbDrop.v
+++ compose/SimSymbDrop.v
@@ -295,6 +295,179 @@ Lemma or_des A B (OR: A \/ B):
   (A /\ B) \/ (A /\ ~ B) \/ (~ A /\ B).
 Proof. tauto. Qed.
 
+Let init_meminj (sk_src sk_tgt:Sk.t) : meminj :=
+  fun b =>
+    match Genv.invert_symbol (Sk.load_skenv sk_src) b with
+    | Some id =>
+      match Genv.find_symbol (Sk.load_skenv sk_tgt) id with
+      | Some b' => Some (b', 0)
+      | None => None
+      end
+    | None => None
+    end.
+
+Remark init_meminj_invert
+       sk_src sk_tgt ss
+       b b' delta
+       (INJ: (init_meminj sk_src sk_tgt) b = Some(b', delta))
+       (SIMSK : sim_sk ss sk_src sk_tgt)
+  :
+  delta = 0 /\ exists id, Genv.find_symbol (Sk.load_skenv sk_src) id = Some b /\ Genv.find_symbol (Sk.load_skenv sk_tgt) id = Some b' /\ ~ ss id.
+Proof.
+  unfold init_meminj in *; intros.
+  destruct (Genv.invert_symbol (Sk.load_skenv sk_src) b) as [id|] eqn:S; try discriminate.
+  destruct (Genv.find_symbol (Sk.load_skenv sk_tgt) id) as [b''|] eqn:F; inv INJ.
+  split. auto. exists id. split. apply Genv.invert_find_symbol; auto. split. auto.
+  ii. unfold Sk.load_skenv in *. apply Genv.find_symbol_inversion in F. apply prog_defmap_dom in F. des.
+  inv SIMSK. apply DROP in H. congruence.
+Qed.
+
+Remark init_meminj_eq
+       sk_src sk_tgt ss
+       id b b'
+       (SYMBOL: Genv.find_symbol (Sk.load_skenv sk_src) id = Some b)
+       (SYMBOL2: Genv.find_symbol (Sk.load_skenv sk_tgt) id = Some b')
+       (SIMSK: sim_sk ss sk_src sk_tgt)
+  :
+  (init_meminj sk_src sk_tgt) b = Some(b', 0).
+Proof.
+  intros. unfold init_meminj. erewrite Genv.find_invert_symbol by eauto. rewrite SYMBOL2. auto.
+Qed.
+
+Lemma init_mem_exists
+      sk_src sk_tgt
+      ss m_src
+      (SIMSK: sim_sk ss sk_src sk_tgt)
+      (LOADMEMSRC: Sk.load_mem sk_src = Some m_src)
+  :
+    exists m_tgt, Sk.load_mem sk_tgt = Some m_tgt.
+Proof.
+  inv SIMSK. unfold Sk.load_mem in *. apply Genv.init_mem_exists. i.
+  assert (P: (prog_defmap sk_tgt)!id = Some (Gvar v)).
+  { eapply prog_defmap_norepet; eauto. apply NoDup_norepet. ss. }
+  assert (Q: (prog_defmap sk_src) ! id = Some (Gvar v)).
+  { rewrite <- KEPT; ss. ii. rewrite DROP in P; ss. }
+  exploit Genv.init_mem_inversion; eauto. apply in_prog_defmap; eauto. intros [AL FV].
+  split. auto.
+  intros. exploit FV; eauto. intros (b & FS).
+  eapply Genv.find_symbol_inversion in FS. apply prog_defmap_dom in FS. destruct FS as (g & R).
+  apply Genv.find_symbol_exists with g. apply in_prog_defmap. rewrite KEPT; ss.
+  ii. eapply NOREF; et. r. exists o; et.
+Qed.
+
+Lemma init_meminj_simskenv
+      sk_src sk_tgt ss
+      m_src m_tgt
+      (LOADMEMSRC: Sk.load_mem sk_src = Some m_src)
+      (LOADMEMTGT: Sk.load_mem sk_tgt = Some m_tgt)
+      (SIMSK: sim_sk ss sk_src sk_tgt)
+  : sim_skenv (mk m_src m_tgt (init_meminj sk_src sk_tgt) bot2 bot2 (Mem.nextblock m_src) (Mem.nextblock m_tgt))
+              ss (Sk.load_skenv sk_src) (Sk.load_skenv sk_tgt).
+Proof.
+  econs; ss; i.
+  - exploit init_meminj_invert; eauto. intros (A & id1 & B & C & D).
+    assert (id1 = id) by (eapply (Genv.genv_vars_inj (Sk.load_skenv sk_src)); eauto). subst id1.
+    esplits; auto.
+  - assert(exists blk_tgt : block, Genv.find_symbol (Sk.load_skenv sk_tgt) id = Some blk_tgt).
+    { apply Genv.find_symbol_inversion in BLKSRC. apply prog_defmap_dom in BLKSRC. destruct BLKSRC as (g & P).
+      apply Genv.find_symbol_exists with g. apply in_prog_defmap. inv SIMSK. rewrite KEPT0; ss.
+    }
+    des. exists blk_tgt; split; auto.
+    eapply init_meminj_eq; eauto.
+  - assert(exists blk_src : block, Genv.find_symbol (Sk.load_skenv sk_src) id = Some blk_src).
+    { apply Genv.find_symbol_inversion in BLKTGT. apply prog_defmap_dom in BLKTGT. destruct BLKTGT as (g & P).
+      apply Genv.find_symbol_exists with g. apply in_prog_defmap. inv SIMSK. rewrite <- KEPT; ss.
+      ii. rewrite DROP in P; ss.
+    }
+    des. exists blk_src; split; auto.
+    eapply init_meminj_eq; eauto.
+  - exploit init_meminj_invert; eauto. intros (A & id & B & C & D).
+    assert ((prog_defmap sk_src)!id = Some def_src).
+    { rewrite Genv.find_def_symbol. exists blk_src; auto. }
+    assert ((prog_defmap sk_tgt)!id = Some def_src).
+    { inv SIMSK. rewrite KEPT; ss. }
+    rewrite Genv.find_def_symbol in H0. destruct H0 as (b & P & Q).
+    unfold Sk.load_skenv in *. replace b with blk_tgt in * by congruence. exists def_src. split; auto.
+  - unfold init_meminj in *. des_ifs. apply_all_once Genv.find_invert_symbol. rewrite Heq2 in Heq0. inv Heq0.
+    apply_all_once Genv.invert_find_symbol. congruence.
+  - exploit init_meminj_invert; eauto. intros (A & id & B & C & D).
+    assert ((prog_defmap sk_tgt)!id = Some def_tgt).
+    { rewrite Genv.find_def_symbol. exists blk_tgt; auto. }
+    inv SIMSK. rewrite KEPT in H; ss.
+    rewrite Genv.find_def_symbol in H. destruct H as (b & P & Q).
+    unfold Sk.load_skenv in *. replace b with blk_src in * by congruence. exists def_tgt. split; auto.
+  - ii. inv SIMSK. apply CLOSED in H. unfold privs in *. apply andb_true_iff in H. des.
+    apply negb_true_iff in H0. unfold Sk.load_skenv in *. rewrite Genv.globalenv_public in PR.
+    clear - PR H0. admit "ez".
+  - inv SIMSK. unfold Sk.load_skenv. do 2 rewrite Genv.globalenv_public. ss.
+  - inv SIMSK. erewrite Genv.init_mem_genv_next; et. xomega.
+  - inv SIMSK. erewrite Genv.init_mem_genv_next; et. xomega.
+Qed.
+
+Lemma init_meminj_invert_strong
+      sk_src sk_tgt ss
+      b b' delta
+      (INJ: (init_meminj sk_src sk_tgt) b = Some(b', delta))
+      (SIMSK : sim_sk ss sk_src sk_tgt)
+  :
+    delta = 0 /\
+    exists id gd,
+      Genv.find_symbol (Sk.load_skenv sk_src) id = Some b
+      /\ Genv.find_symbol (Sk.load_skenv sk_tgt) id = Some b'
+      /\ Genv.find_def (Sk.load_skenv sk_src) b = Some gd
+      /\ Genv.find_def (Sk.load_skenv sk_tgt) b' = Some gd.
+Proof.
+  intros. exploit init_meminj_invert; eauto. intros (A & id & B & C & D). unfold Sk.load_skenv in *.
+  assert (exists gd, (prog_defmap sk_src)!id = Some gd).
+  { apply prog_defmap_dom. eapply Genv.find_symbol_inversion; eauto. }
+  destruct H as [gd DM].
+  assert ((prog_defmap sk_tgt)!id = Some gd).
+  { inv SIMSK. rewrite KEPT; ss. }
+  rewrite Genv.find_def_symbol in DM. destruct DM as (b'' & P & Q). rewrite P in B; inv B.
+  rewrite Genv.find_def_symbol in H. destruct H as (b'' & R & S). rewrite R in C; inv C.
+  esplits; et.
+Qed.
+
+Lemma bytes_of_init_inject
+      sk_src sk_tgt ss
+      m_src il
+      (SIMSK: sim_sk ss sk_src sk_tgt)
+      (LOADMEMSRC: Sk.load_mem sk_src = Some m_src)
+      (REF: forall id, ref_init il id -> ~ ss id)
+  :
+    list_forall2 (memval_inject (init_meminj sk_src sk_tgt)) (Genv.bytes_of_init_data_list (Sk.load_skenv sk_src) il) (Genv.bytes_of_init_data_list (Sk.load_skenv sk_tgt) il).
+Proof.
+  exploit init_mem_exists; et. intros LOADMEMTGT; des.
+  induction il as [ | i1 il]; simpl; intros.
+  - constructor.
+  - apply list_forall2_app.
+    + exploit init_meminj_simskenv; try eapply SIMSK; et. i. inv H; ss.
+      destruct i1; simpl; try (apply inj_bytes_inject).
+      { induction (Z.to_nat z); simpl; constructor. constructor. auto. }
+      destruct (Genv.find_symbol (Sk.load_skenv sk_src) i) as [b|] eqn:FS.
+      * assert (~ ss i). { apply REF. red. exists i0; auto with coqlib. }
+        exploit SIMSYMB2; et. intros (b' & A & B). rewrite A. apply inj_value_inject.
+        econstructor; eauto. symmetry; apply Ptrofs.add_zero.
+      * destruct (Genv.find_symbol (Sk.load_skenv sk_tgt) i) as [b'|] eqn:FS'.
+        exploit SIMSYMB3; et.
+        intros (b & A & B). congruence.
+        apply repeat_Undef_inject_self.
+    + apply IHil. intros id [ofs IN]. apply REF. exists ofs; auto with coqlib.
+Qed.
+
+Lemma Mem_getN_forall2:
+  forall (P: memval -> memval -> Prop) c1 c2 i n p,
+  list_forall2 P (Mem.getN n p c1) (Mem.getN n p c2) ->
+  p <= i -> i < p + Z.of_nat n ->
+  P (ZMap.get i c1) (ZMap.get i c2).
+Proof.
+  induction n; simpl Mem.getN; intros.
+- simpl in H1. omegaContradiction.
+- inv H. rewrite Nat2Z.inj_succ in H1. destruct (zeq i p).
++ congruence.
++ apply IHn with (p + 1); auto. omega. omega.
+Qed.
+
 Global Program Instance SimSymbDrop: SimSymb.class SimMemInj := {
   t := t';
   le := le;
@@ -386,12 +559,6 @@ Next Obligation.
       * rewrite DROP0; ss. rewrite KEPT; ss. rewrite AUX1; ss.
     + admit "ez".
     + admit "ez".
-      (* rewrite !prog_defmap_elements, !PTree.gcombine in PROG; ss. *)
-      (* destruct ((prog_defmap sk_tgt0) ! id) eqn:EQN0; destruct ((prog_defmap sk_tgt1) ! id) eqn:EQN1; inv PROG. *)
-      (* * admit "ez". *)
-      (* * apply or_des in DROP1. des; try by (exploit NOREF; et). exploit AUX1; et. i. exploit KEPT; et. congruence. *)
-      (* * *)
-      (* * *)
     + apply NoDup_norepet. apply PTree.elements_keys_norepet.
 (*   admit "See 'link_match_program' in Unusedglobproof.v. *)
 (* Note that we have one more goal (exists ss) but it is OK, as the 'link_match_program' proof already proves it.". *)
@@ -400,30 +567,71 @@ Next Obligation.
   inv SIMSKE. unfold Genv.public_symbol. apply func_ext1. intro i0.
   destruct (Genv.find_symbol skenv_tgt i0) eqn:T.
   - exploit SIMSYMB3; et. i; des. des_ifs. rewrite PUB. ss.
-  -  des_ifs. des_sumbool. ii. eapply PUBKEPT in H. exploit SIMSYMB2; et. i; des. clarify.
+  - des_ifs. des_sumbool. ii. eapply PUBKEPT in H. exploit SIMSYMB2; et. i; des. clarify.
 Qed.
 Next Obligation.
-  assert(LOADMEMTGT: exists m_tgt, Sk.load_mem sk_tgt = Some m_tgt).
-  { inv SIMSK. unfold Sk.load_mem, Genv.init_mem, Genv.globalenv in *.
-    rewrite <- PUB. clear - KEPT DROP LOADMEMSRC. admit "TODO-use induction - Using init_mem_exists".
-  }
-
-  Definition init_meminj sk_src sk_tgt : meminj :=
-  fun b =>
-    match Genv.invert_symbol (Sk.load_skenv sk_src) b with
-    | Some id =>
-        match Genv.find_symbol (Sk.load_skenv sk_tgt) id with
-        | Some b' => Some (b', 0)
-        | None => None
-        end
-    | None => None
-    end.
-
-  des. eexists m_tgt. exists (mk m_src m_tgt (init_meminj sk_src sk_tgt) bot2 bot2 (Mem.nextblock m_src) (Mem.nextblock m_tgt)).
+  exploit init_mem_exists; et. intros LOADMEMTGT; des.
+  exploit init_meminj_simskenv; try eapply SIMSK; et. intros SIMSKENV.
+  eexists m_tgt. exists (mk m_src m_tgt (init_meminj sk_src sk_tgt) bot2 bot2 (Mem.nextblock m_src) (Mem.nextblock m_tgt)).
   esplits; et.
-  - admit "A".
-  - econs; ss; try xomega.
-  admit "See 'init_meminj_preserves_globals' in Unusedglobproof.v".
+  { econs; ss; try xomega. constructor; intros.
+    { intros; constructor; intros.
+      - exploit init_meminj_invert_strong; eauto. intros (A & id & gd & B & C & D & E).
+        exploit (Genv.init_mem_characterization_gen sk_src); eauto.
+        exploit (Genv.init_mem_characterization_gen sk_tgt); eauto.
+        destruct gd as [f|v].
+        + intros (P2 & Q2) (P1 & Q1).
+          apply Q1 in H0. destruct H0. subst.
+          apply Mem.perm_cur. auto.
+        + intros (P2 & Q2 & R2 & S2) (P1 & Q1 & R1 & S1).
+          apply Q1 in H0. destruct H0. subst.
+          apply Mem.perm_cur. eapply Mem.perm_implies; eauto.
+          apply P2. omega.
+      - exploit init_meminj_invert; eauto. intros (A & id & B & C).
+        subst delta. apply Z.divide_0_r.
+      - exploit init_meminj_invert_strong; eauto. intros (A & id & gd & B & C & D & E).
+        exploit (Genv.init_mem_characterization_gen sk_src); eauto.
+        exploit (Genv.init_mem_characterization_gen sk_tgt); eauto.
+        destruct gd as [f|v].
+        + intros (P2 & Q2) (P1 & Q1).
+          apply Q1 in H0. destruct H0; discriminate.
+        + intros (P2 & Q2 & R2 & S2) (P1 & Q1 & R1 & S1).
+          apply Q1 in H0. destruct H0.
+          assert (NO: gvar_volatile v = false).
+          { unfold Genv.perm_globvar in H1. destruct (gvar_volatile v); auto. inv H1. }
+          Local Transparent Mem.loadbytes.
+          generalize (S1 NO). unfold Mem.loadbytes. destruct Mem.range_perm_dec; intros E1; inv E1.
+          generalize (S2 NO). unfold Mem.loadbytes. destruct Mem.range_perm_dec; intros E2; inv E2.
+          rewrite Z.add_0_r.
+          apply Mem_getN_forall2 with (p := 0) (n := nat_of_Z (init_data_list_size (gvar_init v))).
+          rewrite H3, H4. eapply bytes_of_init_inject; et.
+          { ii. inv SIMSK. eapply NOREF; et. eapply Genv.find_def_symbol. eexists. split; et. }
+          omega.
+          rewrite nat_of_Z_eq by (apply init_data_list_size_pos). omega.
+    }
+    - destruct ((init_meminj sk_src sk_tgt) b) as [[b' delta]|] eqn:INJ; auto.
+      elim H. exploit init_meminj_invert; eauto. intros (A & id & B & C & D).
+      unfold Sk.load_skenv, Sk.load_mem in *. eapply Genv.find_symbol_not_fresh; eauto.
+    - exploit init_meminj_invert; eauto. intros (A & id & B & C & D).
+      unfold Sk.load_skenv, Sk.load_mem in *. eapply Genv.find_symbol_not_fresh; eauto.
+    - red; intros.
+      exploit init_meminj_invert; try eapply H0; et. intros (A1 & id1 & B1 & C1 & D1).
+      exploit init_meminj_invert; try eapply H1; et. intros (A2 & id2 & B2 & C2 & D2).
+      destruct (ident_eq id1 id2). congruence. left; eapply Genv.global_addresses_distinct; eauto.
+    - exploit init_meminj_invert; eauto. intros (A & id & B & C & D). subst delta.
+      split. omega. generalize (Ptrofs.unsigned_range_2 ofs). omega.
+    - exploit init_meminj_invert_strong; eauto. intros (A & id & gd & B & C & D & E).
+      exploit (Genv.init_mem_characterization_gen sk_src); eauto.
+      exploit (Genv.init_mem_characterization_gen sk_tgt); eauto.
+      destruct gd as [f|v].
+      + intros (P2 & Q2) (P1 & Q1).
+        apply Q2 in H0. destruct H0. subst. replace ofs with 0 by omega.
+        left; apply Mem.perm_cur; auto.
+      + intros (P2 & Q2 & R2 & S2) (P1 & Q1 & R1 & S1).
+        apply Q2 in H0. destruct H0. subst.
+        left. apply Mem.perm_cur. eapply Mem.perm_implies; eauto.
+        apply P1. omega.
+  }
 Qed.
 Next Obligation.
   inv MLE. inv SIMSKENV.

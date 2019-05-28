59b316b924039a190a31f7e676ddaa41b63c07b8
alxest <youngju.song@sf.snu.ac.kr>
2019-05-28 16:24:43 +0900

    Add LANGS to script

--- count.rb
+++ count.rb
@@ -8,9 +8,13 @@ STACKNIG=["backend/Stackingproof"]
 WHOLE=["x86/Asmgenproof", "cfrontend/Cminorgenproof", "cfrontend/Cshmgenproof", "cfrontend/SimplLocalsproof", "cfrontend/SimplExprproof"]
 #NOTE: Cstrategy is omitted because it contains semantics/other proofs (bigstep) too
 
+LANGS=["cfrontend/Csem", "cfrontend/Clight", "cfrontend/Csharpminor", "backend/Cminor", "backend/CminorSel",
+       "backend/RTL", "backend/LTL", "backend/Linear", "backend/Mach", "x86/Asm"]
+
 RTLS.map!{|i| i + if COMPCOMP then "C.v" else ".v" end}
 BACKENDS.map!{|i| i + if COMPCOMP then "C.v" else ".v" end}
 WHOLE.map!{|i| i + if COMPCOMP then "C.v" else ".v" end}
+LANGS.map!{|i| i + if COMPCOMP then "C.v" else ".v" end}
 
 puts
 puts "<<<RTLS>>>"
@@ -24,5 +28,10 @@ system("tokei -f #{RTLS.join(" ")} #{BACKENDS.join(" ")}")
 
 puts
 puts "<<<WHOLE - STACKING>>>"
+puts
+system("tokei -f #{RTLS.join(" ")} #{BACKENDS.join(" ")} #{WHOLE.join(" ")}")
+
+puts
+puts "<<<LANGS>>>"
 puts
 system("tokei -f #{RTLS.join(" ")} #{BACKENDS.join(" ")} #{WHOLE.join(" ")}")

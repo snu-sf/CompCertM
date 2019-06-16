1ec9470391c7f184c895e2581d2e06d9a71fd2b1
alxest <youngju.song@sf.snu.ac.kr>
2019-06-17 02:00:31 +0900

    compiles

--- demo/mutrec/MutrecABproof2.v
+++ demo/mutrec/MutrecABproof2.v
@@ -206,7 +206,7 @@ Section LXSIM.
         (MATCH: match_states i st_src0 st_tgt0)
     :
       xsim (sem (ctx ++ [module])) (sem (ctx ++ [MutrecAspec.module; MutrecBspec.module]))
-           Z.lt i st_src0 st_tgt0
+           (Zwf.Zwf 0) i st_src0 st_tgt0
   .
   Proof.
     revert_until LINKTGT. pcofix CIH. i.
@@ -302,7 +302,7 @@ Section LXSIM.
                   }
                   ss. econs; ss; eauto.
                   admit "ez - genv g_id some".
-              + instantiate (1:= Int.intval cur - 1). lia.
+              + instantiate (1:= Int.intval cur - 1). rr. esplits; try lia. admit "by definition".
             - right. eapply CIH. econs; eauto.
               assert(ARITH: Int.intval (Int.sub cur (Int.repr 1)) = Int.intval cur - 1).
               { admit "ez - arithmetic. it does not underflow". }
@@ -348,7 +348,7 @@ Section LXSIM.
                   }
                   ss. econs; ss; eauto.
                   admit "ez - genv f_id some".
-              + instantiate (1:= Int.intval cur - 1). lia.
+              + instantiate (1:= Int.intval cur - 1). split; try lia. admit "by definition".
             - right. eapply CIH. econs; eauto.
               assert(ARITH: Int.intval (Int.sub cur (Int.repr 1)) = Int.intval cur - 1).
               { admit "ez - arithmetic. it does not underflow". }
@@ -411,7 +411,7 @@ Section LXSIM.
                   econs; ss; eauto.
                   rewrite Int.add_commut.
                   rewrite Int.sub_add_l. rewrite Int.sub_idem. rewrite Int.add_zero_l. ss.
-                + instantiate (1:= Int.intval max - Int.intval cur - 1). lia.
+                + instantiate (1:= Int.intval max - Int.intval cur - 1). split; try lia.
               - right. eapply CIH; eauto. unfold Frame.update_st. ss. econs; eauto. econs 3; ss.
                 + unfold __GUARD__. eauto.
                 + admit "ez - arithmetic".
@@ -428,7 +428,7 @@ Section LXSIM.
                   econs; ss; eauto.
                   rewrite Int.add_commut.
                   rewrite Int.sub_add_l. rewrite Int.sub_idem. rewrite Int.add_zero_l. ss.
-                + instantiate (1:= Int.intval max - Int.intval cur - 1). lia.
+                + instantiate (1:= Int.intval max - Int.intval cur - 1). split; try lia.
               - right. eapply CIH; eauto. unfold Frame.update_st. ss. econs; eauto. econs 3; ss.
                 + unfold __GUARD__. eauto.
                 + admit "ez - arithmetic".
@@ -448,7 +448,7 @@ Section LXSIM.
                   econs; ss; eauto.
                   rewrite Int.add_commut.
                   rewrite Int.sub_add_l. rewrite Int.sub_idem. rewrite Int.add_zero_l. ss.
-                + instantiate (1:= Int.intval max - Int.intval cur - 1). lia.
+                + instantiate (1:= Int.intval max - Int.intval cur - 1). split; try lia.
               - right. eapply CIH; eauto. unfold Frame.update_st. ss. econs; eauto. econs 3; ss.
                 + unfold __GUARD__. eauto.
                 + admit "ez - arithmetic".
@@ -465,7 +465,7 @@ Section LXSIM.
                   econs; ss; eauto.
                   rewrite Int.add_commut.
                   rewrite Int.sub_add_l. rewrite Int.sub_idem. rewrite Int.add_zero_l. ss.
-                + instantiate (1:= Int.intval max - Int.intval cur - 1). lia.
+                + instantiate (1:= Int.intval max - Int.intval cur - 1). split; try lia.
               - right. eapply CIH; eauto. unfold Frame.update_st. ss. econs; eauto. econs 3; ss.
                 + unfold __GUARD__. eauto.
                 + admit "ez - arithmetic".
@@ -483,9 +483,6 @@ End LXSIM.
 Theorem mutrecABcorrect
         ctx
   :
-    (* (<<REFINE: improves (Sem.sem ([(MutrecABspec.module)] ++ ctx)) *)
-    (*                     (Sem.sem ([(MutrecAspec.module) ; (MutrecBspec.module)] ++ ctx)) *)
-    (*                     >>) *)
     (<<REFINE: improves (Sem.sem (ctx ++ [(MutrecABspec.module)]))
                         (Sem.sem (ctx ++ [(MutrecAspec.module) ; (MutrecBspec.module)]))
                         >>)
@@ -512,327 +509,6 @@ Proof.
         * eapply wf_module_Bspec; et.
     - i; ss. inv INIT0. inv INIT1. clarify.
   }
-  eapply match_states_xsim.
+  eapply match_states_xsim; eauto.
+  econs; eauto. econs; eauto.
 Qed.
-
-
-(* Inductive match_states_internal: MutrecABspec.state -> Clight.state -> Prop := *)
-(* | match_callstate_nonzero *)
-(*     i m_src m_tgt *)
-(*     fptr *)
-(*     (* targs tres cconv *) *)
-(*     (FINDF: Genv.find_funct (Smallstep.globalenv (modsem2 skenv_link prog)) fptr = Some (Internal func_f)) *)
-(*   : *)
-(*     match_states_internal (Callstate i m_src) (Clight.Callstate fptr (Tfunction (* targs tres cconv) *) *)
-(*                                                                         (Tcons tint Tnil) tint cc_default) *)
-(*                                                                 [Vint i] Kstop m_tgt) *)
-(* | match_returnstate *)
-(*     i m_src m_tgt *)
-(*   : *)
-(*     match_states_internal (Returnstate i m_src) (Clight.Returnstate (Vint i) Kstop m_tgt) *)
-(* . *)
-
-(* Inductive match_states (sm_init: SimMem.t) *)
-(*           (idx: nat) (st_src0: MutrecAspec.state) (st_tgt0: Clight.state) (sm0: SimMem.t): Prop := *)
-(* | match_states_intro *)
-(*     (MATCHST: match_states_internal st_src0 st_tgt0) *)
-(*     (MCOMPATSRC: st_src0.(get_mem) = sm0.(SimMem.src)) *)
-(*     (MCOMPATTGT: st_tgt0.(ClightC.get_mem) = sm0.(SimMem.tgt)) *)
-(*     (MWF: SimMem.wf sm0) *)
-(*     (IDX: (idx > 3)%nat) *)
-(* . *)
-
-Theorem make_match_genvs :
-  SimSymbId.sim_skenv (SkEnv.project skenv_link md_src.(Mod.sk)) (SkEnv.project skenv_link md_tgt.(Mod.sk)) ->
-  Genv.match_genvs (match_globdef (fun _ => eq) eq tt) ge tge.
-Proof.
-  subst_locals. ss. ii.
-  eapply SimSymbId.sim_skenv_revive; eauto.
-  admit "ez - reflexivity".
-Qed.
-
-Lemma g_blk_exists
-  :
-    exists g_blk,
-      (<<FINDG: Genv.find_symbol
-                  (SkEnv.revive (SkEnv.project skenv_link (CSk.of_program signature_of_function prog)) prog)
-                  g_id = Some g_blk>>)
-      /\
-      (<<FINDG: Genv.find_funct_ptr
-                  (SkEnv.revive (SkEnv.project skenv_link (CSk.of_program signature_of_function prog)) prog)
-                  g_blk = None>>)
-      /\
-      (<<FINDG: exists skd, Genv.find_funct_ptr skenv_link g_blk = Some skd /\
-                            signature_of_type (Tcons tint Tnil) tint cc_default = SkEnv.get_sig skd>>)
-.
-Proof.
-  exploit (prog_defmap_norepet prog g_id); eauto.
-  { unfold prog_defs_names. ss. repeat (econs; eauto). ii; ss; des; ss. }
-  { ss. eauto. }
-  intro T; des.
-  exploit SkEnv.project_impl_spec; eauto. intro PROJ.
-  assert(PREC: SkEnv.genv_precise
-                 (SkEnv.revive (SkEnv.project skenv_link (CSk.of_program signature_of_function prog)) prog)
-                 prog).
-  { eapply CSkEnv.project_revive_precise; ss; et. }
-  inv PREC.
-  exploit (P2GE g_id); eauto. i; des. des_ifs.
-  rename b into g_blk.
-  eexists. splits; et.
-  { unfold Genv.find_funct_ptr. des_ifs. }
-  (* exploit (@SkEnv.project_revive_precise _ _ skenv_link); eauto. *)
-  { inv INCLSRC.
-    exploit (CSk.of_program_prog_defmap prog signature_of_function); et. rewrite T. intro S.
-
-    remember ((prog_defmap (CSk.of_program signature_of_function prog)) ! g_id) as U in *.
-    destruct U eqn:V; try (by ss). inv S. inv H1.
-
-    exploit DEFS; eauto. i; des.
-    assert(blk = g_blk).
-    { inv PROJ. exploit SYMBKEEP; et.
-      - instantiate (1:= g_id). unfold defs. des_sumbool. ss. et.
-      - i. rewrite SYMB0 in *. clear - SYMB H. unfold SkEnv.revive in *. rewrite Genv_map_defs_symb in *. ss.
-        rewrite SYMB in *. des. clarify.
-    }
-    clarify. inv MATCH.
-    esplits; eauto.
-    - unfold Genv.find_funct_ptr. rewrite DEF0. et.
-    - ss. des_ifs. clear - H1. inv H1; ss.
-  }
-Qed.
-
-Lemma match_states_lxsim
-      sm_init idx st_src0 st_tgt0 sm0
-      (MATCH: match_states sm_init idx st_src0 st_tgt0 sm0)
-  :
-    <<XSIM: lxsim (md_src skenv_link) (md_tgt skenv_link)
-                  (fun st => exists su m_init, SoundTop.sound_state su m_init st)
-                  sm_init (Ord.lift_idx lt_wf idx) st_src0 st_tgt0 sm0>>
-.
-Proof.
-  revert_until tge.
-  pcofix CIH.
-  i.
-  pfold.
-  generalize g_blk_exists; et. i; des.
-  inv MATCH. ss. inv MATCHST; ss; clarify.
-  - (* call *)
-    destruct (classic (i = Int.zero)).
-    + (* zero *)
-      clarify.
-      econs 1. i; des.
-      econs 1; cycle 2.
-      { admit "ez - spec is receptive". }
-      { split; ii; rr in H; inv H; inv H0; ss. }
-      i. ss. inv STEPSRC.
-      esplits; eauto.
-      * left.
-        eapply plus_left with (t1 := E0) (t2 := E0); ss.
-        { econs; eauto.
-          { eapply modsem2_determinate; eauto. }
-          econs; eauto.
-          econs; ss; eauto; try (by repeat (econs; ss; eauto)).
-          unfold _x. unfold _t'1. rr. ii; ss. des; ss. clarify.
-        }
-
-        eapply star_left with (t1 := E0) (t2 := E0); ss.
-        { econs; eauto.
-          { eapply modsem2_determinate; eauto. }
-          econs; eauto.
-        }
-
-        eapply star_left with (t1 := E0) (t2 := E0); ss.
-        { econs; eauto.
-          { eapply modsem2_determinate; eauto. }
-          econs; eauto.
-          - repeat econs; et.
-          - ss.
-        }
-
-        eapply star_left with (t1 := E0) (t2 := E0); ss.
-        { econs; eauto.
-          { eapply modsem2_determinate; eauto. }
-          econs; eauto.
-          - repeat econs; et.
-          - ss.
-          - ss.
-        }
-
-        apply star_refl.
-      (* * refl. *)
-      * right. eapply CIH. econs; ss; eauto. econs; eauto.
-    + (* nonzero *)
-      econs.
-      i; des.
-      econs 2; eauto.
-      * esplits; cycle 1.
-        { eapply Ord.lift_idx_spec. instantiate (1:= 2%nat). lia. }
-
-        eapply plus_left with (t1 := E0) (t2 := E0); ss.
-        { econs; eauto.
-          { eapply modsem2_determinate; eauto. }
-          econs; eauto.
-          econs; ss; eauto; try (by repeat (econs; ss; eauto)).
-          unfold _x. unfold _t'1. rr. ii; ss. des; ss. clarify.
-        }
-        
-        eapply star_left with (t1 := E0) (t2 := E0); ss.
-        { econs; eauto.
-          { eapply modsem2_determinate; eauto. }
-          econs; eauto.
-        }
-
-        eapply star_left with (t1 := E0) (t2 := E0); ss.
-        { econs; eauto.
-          { eapply modsem2_determinate; eauto. }
-          econs; eauto.
-          - repeat econs; et.
-          - ss. rewrite Int.eq_false; ss.
-        }
-
-        eapply star_left with (t1 := E0) (t2 := E0); ss.
-        { econs; eauto.
-          { eapply modsem2_determinate; eauto. }
-          econs; eauto.
-        }
-
-        eapply star_left with (t1 := E0) (t2 := E0); ss.
-        { econs; eauto.
-          { eapply modsem2_determinate; eauto. }
-          econs; eauto.
-        }
-
-        eapply star_left with (t1 := E0) (t2 := E0); ss.
-        { econs; eauto.
-          { eapply modsem2_determinate; eauto. }
-          econs; eauto; swap 1 2.
-          - econs.
-            + eapply eval_Evar_global; ss. et.
-            + econs 2; et.
-          - unfold Cop.classify_fun. ss.
-          - repeat econs; ss; et.
-        }
-
-        apply star_refl.
-      * left. pfold. econs 3; et.
-        { rr. esplits; eauto. ss. econs; et. ii. destruct i; ss. clarify. apply H. unfold Int.zero.
-          Local Transparent Int.repr.
-          unfold Int.repr.
-          Local Opaque Int.repr.
-          ss. apply eta; ss.
-        }
-        ii; des.
-        inv ATSRC. ss; clarify.
-        destruct sm0; ss. clarify.
-        unfold Clight.fundef in *.
-        rewrite FINDG in *. clarify.
-        eexists (Args.mk _ [Vint (Int.sub i (Int.repr 1))] _), (SimMemId.mk _ _).
-        esplits; ss; eauto.
-        { econs; ss; eauto. }
-        i. inv AFTERSRC. destruct retv_src, retv_tgt; ss. clarify. destruct sm_ret; ss. inv SIMRETV; ss; clarify.
-        esplits; eauto.
-        { econs; eauto. }
-        instantiate (2:= (Ord.lift_idx lt_wf 15%nat)).
-        left. pfold. econs; eauto. i; des. econs 2; eauto.
-        {
-          esplits; eauto; cycle 1.
-          { instantiate (1:= (Ord.lift_idx lt_wf 14%nat)). eapply Ord.lift_idx_spec; et. }
-
-          eapply plus_left with (t1 := E0) (t2 := E0); ss.
-          { econs; eauto.
-            { eapply modsem2_determinate; eauto. }
-            econs; eauto.
-          }
-          
-          eapply star_left with (t1 := E0) (t2 := E0); ss.
-          { econs; eauto.
-            { eapply modsem2_determinate; eauto. }
-            econs; eauto.
-          }
-
-          eapply star_left with (t1 := E0) (t2 := E0); ss.
-          { econs; eauto.
-            { eapply modsem2_determinate; eauto. }
-            econs; ss; eauto.
-            - repeat (econs; ss; eauto).
-              + unfold typify. des_ifs.
-              + ss.
-            - ss.
-          }
-
-          eapply star_refl.
-        }
-        
-        right. eapply CIH. instantiate (1:= SimMemId.mk _ _).
-        econs; ss; eauto; try lia.
-        rewrite sum_recurse. des_ifs.
-        { rewrite Z.eqb_eq in *. lia. }
-        replace (Int.sub (Int.add (sum (Int.sub i Int.one)) i) (Int.repr 1)) with
-            (Int.add (sum (Int.sub i Int.one)) (Int.sub i Int.one)); cycle 1.
-        { abstr (sum (Int.sub i Int.one)) z. rewrite ! Int.sub_add_opp.
-          rewrite Int.add_assoc. ss. }
-        econs; eauto.
-  - (* return *)
-    econs 4; ss; eauto.
-Unshelve.
-  all: ss.
-Qed.
-
-Theorem sim_modsem
-  :
-    ModSemPair.sim msp
-.
-Proof.
-  econs; eauto.
-  { eapply SoundTop.sound_state_local_preservation. }
-  i. ss. esplits; eauto; cycle 1.
-  { (* init progress *)
-    i.
-    des. inv SAFESRC.
-    inv SIMARGS; ss.
-    (* hexploit (SimMemInjC.skenv_inject_revive prog); et. { apply SIMSKENV. } intro SIMSKENV0; des. *)
-    exploit make_match_genvs; eauto. { apply SIMSKENV. } intro SIMGE.
-
-    (* hexploit (@fsim_external_inject_eq); try apply FINDF; eauto. clear FPTR. intro FPTR. *)
-
-    esplits; eauto. econs; eauto.
-    + rewrite <- FPTR. eauto.
-    + instantiate (1:= [Vint i]). rewrite VS in *. inv VALS. econs; ss.
-      cbn. unfold typify. des_ifs; ss.
-  }
-  (* init bsim *)
-  ii.
-  destruct sm_arg; ss. clarify.
-  clear MFUTURE.
-  inv SIMARGS; ss. clarify.
-  inv INITTGT.
-  (* hexploit (SimMemInjC.skenv_inject_revive prog); et. { apply SIMSKENV. } intro SIMSKENV0; des. *)
-  exploit make_match_genvs; eauto. { apply SIMSKENV. } intro SIMGE. des.
-  eexists. eexists (SimMemId.mk _ _).
-  esplits; eauto.
-  + eapply match_states_lxsim.
-    econs; ss; eauto; cycle 1.
-    { inv SAFESRC. ss. }
-
-    inv SAFESRC. destruct args_src, args_tgt; ss. clarify.
-    assert(tvs = [Vint i]).
-    {
-      unfold signature_of_function in TYP. ss.
-      inv TYP. ss. cbn. unfold typify. des_ifs; ss.
-    } clarify.
-    econs; eauto.
-Unshelve.
-  all: try (ss; econs).
-Qed.
-
-End SIMMODSEM.
-
-Theorem sim_mod
-  :
-    ModPair.sim (ModPair.mk (MutrecAspec.module prog) (ClightC.module2 prog) tt)
-.
-Proof.
-  econs; ss.
-  - ii. inv SIMSKENVLINK. eapply sim_modsem; eauto.
-Qed.
-

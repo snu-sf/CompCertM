9b8deb47ea622f7afe43f6a83428af09d9daea44
alxest <youngju.song@sf.snu.ac.kr>
2019-06-22 19:31:46 +0900

    Remove get_greatest from AdequacySound with few admits

--- proof/AdequacySound.v
+++ proof/AdequacySound.v
@@ -87,84 +87,106 @@ Section ADQSOUND.
   Qed.
 
   (* stack can go preservation when su0 is given *)
-  Inductive sound_stack (su0: Sound.t) (args: Args.t): list Frame.t -> Prop :=
+  Inductive sound_stack (args: Args.t): list Frame.t -> Prop :=
   | sound_stack_nil
-      (GE: sound_ge su0 args.(Args.m))
+      (FORALLSU: forall
+          su0
+          (SUARGS: Sound.args su0 args)
+        ,
+          <<GE: sound_ge su0 args.(Args.m)>>)
+      (EXSU: exists su_ex, Sound.args su_ex args)
     :
-      sound_stack su0 args []
+      sound_stack args []
   | sound_stack_cons
-      su_tail args_tail tail
+      args_tail tail
+      (TL: sound_stack args_tail tail)
       ms lst0
-      (TL: sound_stack su_tail args_tail tail)
-      (HD: forall
-          sound_state_all
-          (PRSV: local_preservation ms sound_state_all)
+      (FORALLSU: forall
+          su0
+          (SUARGS: Sound.args su0 args_tail)
         ,
-          <<SUST: sound_state_all su0 args_tail.(Args.m) lst0>>)
-      (K: forall
-          sound_state_all
-          (PRSV: local_preservation ms sound_state_all)
-        ,
-          (* (<<SUST: sound_state_all su0 args.(Args.m) lst0>>) *)
-          (* /\ *)
+          (<<HD: forall
+                 sound_state_all
+                 (PRSV: local_preservation ms sound_state_all)
+               ,
+                 <<SUST: sound_state_all su0 args_tail.(Args.m) lst0>>>>)
+          /\
           (<<K: forall
-              retv lst1
-              su_greatest
-              (GR: Sound.get_greatest su0 args su_greatest)
-              su_ret
-              (LE: Sound.hle su_greatest su_ret)
-              (SURETV: Sound.retv su_ret retv)
-              (MLE: Sound.mle su_greatest args.(Args.m) retv.(Retv.m))
-              (AFTER: ms.(ModSem.after_external) lst0 retv lst1)
-            ,
-              (* sound_state_all su0 args.(Args.m) lst1>>) *)
-              sound_state_all su0 args_tail.(Args.m) lst1>>)
+                 sound_state_all
+                 (PRSV: local_preservation ms sound_state_all)
+               ,
+                 (* (<<SUST: sound_state_all su0 args.(Args.m) lst0>>) *)
+                 (* /\ *)
+                 exists su_gr,
+                   (<<ARGS: Sound.args su_gr args>>) /\
+                   (<<LE: Sound.le su0 su_gr>>) /\
+                   (<<K: forall
+                       retv lst1
+                       su_ret
+                       (LE: Sound.hle su_gr su_ret)
+                       (SURETV: Sound.retv su_ret retv)
+                       (MLE: Sound.mle su_gr args.(Args.m) retv.(Retv.m))
+                       (AFTER: ms.(ModSem.after_external) lst0 retv lst1)
+                     ,
+                       (* sound_state_all su0 args.(Args.m) lst1>>) *)
+                       sound_state_all su0 args_tail.(Args.m) lst1>>)
+             >>)
+            /\
+            (<<GE: sound_ge su0 args.(Args.m)>>)
       )
-      (GR: Sound.get_greatest su_tail args_tail su0)
+      (EXSU: exists su_ex, Sound.args su_ex args_tail)
       (EX: exists sound_state_ex, local_preservation ms sound_state_ex)
-      (GE: sound_ge su0 args.(Args.m))
       (* sound_state_ex *)
       (* (PRSV: local_preservation ms sound_state_ex) *)
       (* (HD: sound_state_ex su0 lst0) *)
     :
-      sound_stack su0 args ((Frame.mk ms lst0) :: tail)
+      sound_stack args ((Frame.mk ms lst0) :: tail)
   .
 
-  Inductive sound_state (su0: Sound.t): mem -> state -> Prop :=
+  Inductive sound_state: mem -> state -> Prop :=
   | sound_state_normal
-      su_tail args_tail tail
+      args_tail tail
+      (TL: sound_stack args_tail tail)
       ms lst0
       m_arg
-      (GR: Sound.get_greatest su_tail args_tail su0)
-      (TL: sound_stack su_tail args_tail tail)
-      (HD: forall
-          sound_state_all
-          (PRSV: local_preservation ms sound_state_all)
+      (EXSU: exists su_ex, Sound.args su_ex args_tail)
+      (FORALLSU: forall
+          su0
+          (SUARGS: Sound.args su0 args_tail)
         ,
-          <<SUST: sound_state_all su0 m_arg lst0>>)
+          (<<HD: forall
+              sound_state_all
+              (PRSV: local_preservation ms sound_state_all)
+            ,
+              <<SUST: sound_state_all su0 m_arg lst0>>>>)
+          /\
+          (<<GE: sound_ge su0 m_arg>>))
       (EX: exists sound_state_ex, local_preservation ms sound_state_ex)
       (ABCD: args_tail.(Args.m) = m_arg)
-      (GE: sound_ge su0 m_arg)
     :
-      sound_state su0 m_arg (State ((Frame.mk ms lst0) :: tail))
+      sound_state m_arg (State ((Frame.mk ms lst0) :: tail))
   | sound_state_call
-      su_tail m_tail frs
+      m_tail frs
       args
-      (GR: Sound.get_greatest su_tail args su0)
-      (ARGS: Sound.args su0 args)
-      (STK: sound_stack su_tail args frs)
+      (* (ARGS: Sound.args su0 args) *)
+      (STK: sound_stack args frs)
       (* (MLE: Sound.mle su0 m_tail args.(Args.m)) *)
       (EQ: args.(Args.m) = m_tail)
-      (GE: sound_ge su0 m_tail)
+      (EXSU: exists su_ex, Sound.args su_ex args)
+      (FORALLSU: forall
+          su0
+          (SUARGS: Sound.args su0 args)
+        ,
+          (<<GE: sound_ge su0 m_tail>>))
     :
-      sound_state su0 m_tail (Callstate args frs)
+      sound_state m_tail (Callstate args frs)
   .
 
   Lemma sound_init
         st0
         (INIT: sem_src.(Smallstep.initial_state) st0)
     :
-      exists su0 m_init0, <<MEM: Sk.load_mem sk_link_src = Some m_init0>> /\ <<SU: sound_state su0 m_init0 st0>>
+      exists m_init0, <<MEM: Sk.load_mem sk_link_src = Some m_init0>> /\ <<SU: sound_state m_init0 st0>>
   .
   Proof.
     inv INIT. clarify. clear skenv_link_tgt p_tgt skenv_link_tgt sem_tgt LINKTGT INCLTGT WFSKTGT SIMSKENV.
@@ -190,78 +212,49 @@ Section ADQSOUND.
         rewrite <- Mod.get_modsem_skenv_spec; ss. eapply SkEnv.project_impl_spec; et.
     }
     econs; eauto.
-    - eapply Sound.greatest_adq; eauto.
-    - econs; eauto.
-    - eapply vle_preserves_sound_ge; eauto.
-      eapply Sound.greatest_adq; eauto.
+    - econs; eauto. ss. ii. admit "sound-ge".
+    - admit "sound-ge".
+    (* - eapply Sound.greatest_adq; eauto. *)
+    (* - econs; eauto. *)
+    (* - eapply vle_preserves_sound_ge; eauto. *)
+    (*   eapply Sound.greatest_adq; eauto. *)
   Unshelve.
     all: ss.
   Qed.
 
   Lemma sound_progress
-        su0 st0 m_arg0 tr st1
-        (SUST: sound_state su0 m_arg0 st0)
+        st0 m_arg0 tr st1
+        (SUST: sound_state m_arg0 st0)
         (STEP: Step sem_src st0 tr st1)
     :
-      <<SUST: exists su1 m_arg1, sound_state su1 m_arg1 st1>>
+      <<SUST: exists m_arg1, sound_state m_arg1 st1>>
   .
   Proof.
     inv STEP.
     - (* CALL *)
-      inv SUST. ss.
-      exploit (@Sound.greatest_ex _ su0 args); eauto.
-      { des. dup EX. inv EX. exploit CALL; eauto.
-        { eapply HD; eauto. }
-        i; des.
-        eapply Sound.greatest_adq in GR0. des.
-        esplits; eauto.
-      }
-      i; des.
-      generalize (@Sound.greatest_ex _ su0 args). i; des.
-      exists su_gr.
-      exists args.(Args.m).
-      esplits; eauto.
-      econs; eauto; swap 2 3.
-      { eapply Sound.greatest_adq; eauto. }
-      { dup EX. inv EX. exploit CALL; eauto.
-        { eapply HD; eauto. }
-        i; des.
-        eapply vle_preserves_sound_ge; eauto.
-        - eapply mle_preserves_sound_ge; eauto.
-        - eapply Sound.greatest_adq; eauto.
-      }
-      econs; eauto.
-      {
-        ii. dup PRSV. bar. inv PRSV. exploit CALL; eauto; cycle 1.
-        { i; des.
-          assert(su_gr0 = su_greatest).
-          { eapply Sound.greatest_dtm; eauto. }
-          clarify.
-          eapply K; eauto.
-        }
-        eapply HD; eauto.
-      }
-      { eapply mle_preserves_sound_ge; eauto.
-        dup EX. inv EX. exploit CALL; eauto.
-        { eapply HD; eauto. }
-        i; des.
-        ss.
-      }
-      (* intros ? ?. *)
-      (* bar. dup PRSV. inv PRSV. bar. exploit CALL; eauto. { eapply HD; eauto. } i; des. *)
-      (* dsplits; eauto. *)
-      (* ii. *)
-      (* assert(su_greatest = su_gr0). *)
-      (* { eapply Sound.greatest_dtm; eauto. } *)
-      (* clarify. *)
-      (* eapply K; eauto. *)
+      inv SUST. ss. des. exploit FORALLSU; eauto. intro T; des.
+      exploit HD; eauto. i; des. inv EX. exploit CALL; eauto. i; des.
+      esplits; eauto. econs; eauto; cycle 1.
+      + admit "sound-ge".
+      + econs; eauto; cycle 1.
+        { esplits; eauto. econs; eauto. }
+        ii. esplits; eauto.
+        * ii. exploit FORALLSU; try apply SUARGS; eauto. intro U; des. eapply HD0; eauto.
+        * ii. exploit FORALLSU; try apply SUARGS; eauto. intro U; des.
+          exploit HD0; eauto. intro U; des.
+          inv PRSV. exploit CALL0; eauto. i; des. esplits; eauto.
+          ii. eapply K0; eauto.
+        * admit "sound-ge".
     - (* INIT *)
       inv SUST. ss. des_ifs.
-
       esplits; eauto.
       econs; eauto.
-      + ii. inv PRSV. exploit INIT0; eauto.
-        { inv GE. rewrite Forall_forall in *. eapply GE0. inv MSFIND. ss. des_ifs. }
+      + ii. esplits; eauto.
+        * ii. inv PRSV. eapply INIT0; eauto.
+          exploit FORALLSU; eauto. intro U.
+          inv U. rewrite Forall_forall in *. eapply GE; eauto.
+          inv MSFIND. ss. des_ifs.
+        * admit "sound-ge".
       + inv MSFIND. ss. rr in SIMPROG. rewrite Forall_forall in *.
         des; clarify.
         { eapply system_local_preservation. }
@@ -283,8 +276,9 @@ Section ADQSOUND.
     - (* INTERNAL *)
       inv SUST. ss.
       esplits; eauto. econs; eauto.
-      i. dup PRSV. inv PRSV. eapply STEP; eauto.
-      + eapply HD; eauto.
+      i. exploit FORALLSU; eauto. intro U; des. esplits; eauto. i. ss. inv PRSV.
+      eapply STEP; eauto.
+      + eapply HD; et. econs; et.
       + split; ii; ModSem.tac.
     - (* RETURN *)
       inv SUST. ss. rename ms into ms_top.
@@ -293,45 +287,42 @@ Section ADQSOUND.
       inv TL. ss.
       unfold Frame.update_st. s.
       bar.
-      des. dup EX. inv EX. exploit RET; eauto.
-      { eapply HD; eauto. }
-      bar.
-      i; des.
-      esplits; eauto. econs; try apply TL0; eauto.
-      + ii. specialize (K sound_state_all PRSV).
-        eapply K; eauto.
-      +
-        inv TL0; ss.
-        { eapply vle_preserves_sound_ge; eauto.
-          eapply Sound.greatest_adq; eauto.
-        }
-        eapply vle_preserves_sound_ge; eauto.
-        eapply Sound.greatest_adq; eauto.
+      des.
+      esplits; eauto. econs; eauto.
+      ii. esplits; eauto.
+      + ii.
+        exploit FORALLSU0; eauto. i; des. exploit K; eauto. i; des.
+        bar.
+        inv EX.
+        exploit RET; eauto.
+        { eapply FORALLSU; eauto. econs; et. }
+        i; des.
+        eapply K0; eauto.
+      + admit "sound-ge".
   Unshelve.
-    all: ss.
+    ss.
   Qed.
 
   Lemma sound_progress_star
-        su0 st0 m_arg0 tr st1
-        (SUST: sound_state su0 m_arg0 st0)
+        st0 m_arg0 tr st1
+        (SUST: sound_state m_arg0 st0)
         (STEP: Star sem_src st0 tr st1)
     :
-      <<SUST: exists su1 m_arg1, sound_state su1 m_arg1 st1>>
+      <<SUST: exists m_arg1, sound_state m_arg1 st1>>
   .
   Proof.
     generalize dependent m_arg0.
-    generalize dependent su0.
     induction STEP.
     - esplits; eauto.
     - clarify. i. exploit sound_progress; eauto. i; des. eapply IHSTEP; eauto.
   Qed.
 
   Lemma sound_progress_plus
-        su0 st0 m_arg0 tr st1
-        (SUST: sound_state su0 m_arg0 st0)
+        st0 m_arg0 tr st1
+        (SUST: sound_state m_arg0 st0)
         (STEP: Plus sem_src st0 tr st1)
     :
-      <<SUST: exists su1 m_arg1, sound_state su1 m_arg1 st1>>
+      <<SUST: exists m_arg1, sound_state m_arg1 st1>>
   .
   Proof.
     eapply sound_progress_star; eauto. eapply plus_star; eauto.

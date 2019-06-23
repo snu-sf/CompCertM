4062520aac6f8e8f222a807840132d35870c80a7
alxest <youngju.song@sf.snu.ac.kr>
2019-06-23 16:34:51 +0900

    Add staticmutrecB

---
+++ demo/mutrec/StaticMutrecB.v
@@ -0,0 +1,24 @@
+Require Import List ZArith.
+Require Import CoqlibC Integers Floats AST Cop Asm.
+Require Import MutrecHeader.
+
+Set Implicit Arguments.
+
+Local Open Scope Z_scope.
+
+Definition func_g: function := (admit "").
+Definition _memoized : ident := 52554%positive.
+
+Definition global_definitions : list (ident * globdef fundef unit) :=
+((f_id,
+   Gfun(External (EF_external "f"
+                   (mksignature (AST.Tint :: nil) (Some AST.Tint) cc_default))))
+   :: (_memoized, Gvar (admit ""))
+   :: (g_id, Gfun(Internal func_g)) :: nil).
+
+Definition public_idents : list ident :=
+(f_id :: g_id :: nil).
+
+Definition prog : Asm.program := 
+  mkprogram global_definitions public_idents main_id.
+

f2cfb041f311df95b1fd1e64c329ab6f105650a7
Dongjoo-Kim <kimdj316@naver.com>
2019-07-10 23:33:28 +0900

    Fix script for tex

--- scripts/line_count/CompCertM-linking-tex.rb
+++ scripts/line_count/CompCertM-linking-tex.rb
@@ -7,6 +7,10 @@ def print_result(result)
   return result
 end
 
+tex1="Pass Proofs    & 34376     & 35893 (+4.41\\\%)  & "
+tex2="The Rest       & 85617     & 87965 (+2.74\\\%)  & "
+tex3="Whole          & 119993    & 123858 (+3.22\\\%) & "
+
 puts "Count MultiComp"
 puts
 
@@ -22,127 +26,147 @@ PASS_PROOFS.map!{|i| i + "C.v"}
 
 loc=`find . ! -path '*demo/*' -name '*.v' ! -name 'UpperBound_AExtra.v' ! -name 'UpperBound_A.v' ! -name 'UpperBound_B.v' | xargs coqwc`.split("\n")[-1].split(" ")
 loc=loc[0].to_i + loc[1].to_i
+tex3=tex3 + loc.to_s + "(+" + (((loc/119993.0)*100).round(2)).to_s + "\\\%) & 2102 & "
 
 puts
 puts "<<<PASS_PROOFS>>>"
 puts
-loc=loc-print_result(`coqwc #{PASS_PROOFS.join(" ")}`)
+temp=print_result(`coqwc #{PASS_PROOFS.join(" ")}`)
+loc=loc-temp
+tex1=tex1 + temp.to_s + "(+" + (((temp/34376.0)*100).round(2)).to_s + "\\\%) & 1842 & "
+tex2=tex2 + loc.to_s + "(+" + (((loc/85617.0)*100).round(2)).to_s + "\\\%) & 260 & "
 
 puts
 puts "<<Unreadglob-PASS PROOF>>"
 puts
-print_result(`coqwc #{UNREADGLOBPROOFS.map{|i| "demo/unreadglob/" + i + ".v"}.join(" ")}`)
+temp=print_result(`coqwc #{UNREADGLOBPROOFS.map{|i| "demo/unreadglob/" + i + ".v"}.join(" ")}`)
+tex1=tex1 + temp.to_s + " & "
 
 puts
 puts "<<Unreadglob-rest>>"
 puts
-print_result(`find demo/mutrec #{UNREADGLOBPROOFS.inject(""){|sum, i| sum + "! -name \'" + i + ".v\' "}} -name '*.v' | xargs coqwc`)
+temp=print_result(`find demo/mutrec #{UNREADGLOBPROOFS.inject(""){|sum, i| sum + "! -name \'" + i + ".v\' "}} -name '*.v' | xargs coqwc`)
+tex2=tex2 + temp.to_s + " & "
 
 puts
 puts "<<Unreadglob-whole>>"
 puts
-print_result(`coqwc demo/unreadglob/*.v`)
+temp=print_result(`coqwc demo/unreadglob/*.v`)
+tex3=tex3 + temp.to_s + " & "
 
 MUTRECPROOFS=["MutrecAproof", "MutrecBproof", "MutrecABproof"]
 
 puts
 puts "<<Mutrec-PASS PROOF>>"
 puts
-print_result(`coqwc #{MUTRECPROOFS.map{|i| "demo/mutrec/" + i + ".v"}.join(" ")}`)
+temp=print_result(`coqwc #{MUTRECPROOFS.map{|i| "demo/mutrec/" + i + ".v"}.join(" ")}`)
+tex1=tex1 + temp.to_s + " & "
 
 puts
 puts "<<Mutrec-rest>>"
 puts
-print_result(`find demo/mutrec #{MUTRECPROOFS.inject(""){|sum, i| sum + "! -name \'" + i + ".v\' "}} -name '*.v' | xargs coqwc`)
+temp=print_result(`find demo/mutrec #{MUTRECPROOFS.inject(""){|sum, i| sum + "! -name \'" + i + ".v\' "}} -name '*.v' | xargs coqwc`)
+tex2=tex2 + temp.to_s + " & "
 
 puts
 puts "<<Mutrec-whole>>"
 puts
-print_result(`coqwc demo/mutrec/*.v`)
+temp=print_result(`coqwc demo/mutrec/*.v`)
+tex3=tex3 + temp.to_s + " & "
 
 UTODPROOFS=["DemoSpecProof"]
 
 puts
 puts "<<Utod-PASS PROOF>>"
 puts
-print_result(`coqwc #{UTODPROOFS.map{|i| "demo/utod/" + i + ".v"}.join(" ")}`)
+temp=print_result(`coqwc #{UTODPROOFS.map{|i| "demo/utod/" + i + ".v"}.join(" ")}`)
+tex1=tex1 + temp.to_s + " & . \\\\"
 
 puts
 puts "<<Utod-rest>>"
 puts
-print_result(`find demo/utod #{UTODPROOFS.inject(""){|sum, i| sum + "! -name \'" + i + ".v\' "}} -name '*.v' | xargs coqwc`)
+temp=print_result(`find demo/utod #{UTODPROOFS.inject(""){|sum, i| sum + "! -name \'" + i + ".v\' "}} -name '*.v' | xargs coqwc`)
+tex2=tex2 + temp.to_s + " & . \\\\"
 
 puts
 puts "<<Utod-whole>>"
 puts
-print_result(`coqwc demo/utod/*.v`)
+temp=print_result(`coqwc demo/utod/*.v`)
+tex3=tex3 + temp.to_s + " & "
 
 puts
 puts "<<Adq. w.r.t. C>>"
 puts
-print_result(`coqwc bound/UpperBound_AExtra.v bound/UpperBound_A.v bound/UpperBound_B.v`)
+temp=print_result(`coqwc bound/UpperBound_AExtra.v bound/UpperBound_A.v bound/UpperBound_B.v`)
+tex3=tex3 + temp.to_s + " \\\\ "
 
-tex = "Interaction Semantics & "
+breakdown = "Interaction Semantics & "
 puts
 puts "<<Interation Semantics>>"
 puts
 temp=print_result(`coqwc compose/*.v`)
 loc=loc-temp
-tex = tex + temp.to_s + " \\\\ \n"
+breakdown = breakdown + temp.to_s + " \\\\ \n"
 
-tex = tex + "Language Semantics & "
+breakdown = breakdown + "Language Semantics & "
 puts
 puts "<<Language Semantics>>"
 puts
 temp=print_result(`coqwc cfrontend/CsemC.v cfrontend/CstrategyC.v cfrontend/ClightC.v cfrontend/CsharpminorC.v \
                          backend/CminorC.v backend/CminorSelC.v backend/RTLC.v backend/LTLC.v backend/LinearC.v backend/MachC.v x86/AsmC.v`)
 loc=loc-temp
-tex = tex + temp.to_s + " \\\\ \n"
+breakdown = breakdown + temp.to_s + " \\\\ \n"
 
-tex = tex + "Self Simulation & "
+breakdown = breakdown + "Self Simulation & "
 puts
 puts "<<Self Simulation>>"
 puts
 temp=print_result(`coqwc selfsim/*.v`)
 loc=loc-temp
-tex = tex + temp.to_s + " \\\\ \n"
+breakdown = breakdown + temp.to_s + " \\\\ \n"
 
-tex = tex + "CompCert Meta & FILL_THIS \\\\ \n"
+breakdown = breakdown + "CompCert Meta & FILL_THIS \\\\ \n"
 puts
 puts "<<CompCert Meta: the Rest>>"
 puts
 
-tex = tex + "CompCertM Meta & "
+breakdown = breakdown + "CompCertM Meta & "
 puts
 puts "<<CompCertM meta>>"
 puts
 temp=print_result(`find proof -name '*.v' ! -name 'Simulation.v' | xargs coqwc`)
 loc=loc-temp
-tex = tex + temp.to_s + " \\\\ \n"
+breakdown = breakdown + temp.to_s + " \\\\ \n"
 
-tex = tex + "Mixed Simulation & "
+breakdown = breakdown + "Mixed Simulation & "
 puts
 puts "<<Mixed Simulation>>"
 puts
 temp=print_result(`coqwc proof/Simulation.v`)
 loc=loc-temp
-tex = tex + temp.to_s + " \\\\ \n"
+breakdown = breakdown + temp.to_s + " \\\\ \n"
 
-tex = tex + "Adq. w.r.t. Asm & "
+breakdown = breakdown + "Adq. w.r.t. Asm & "
 puts
 puts "<<Adq. w.r.t. Asm>>"
 puts
 temp=print_result(`coqwc bound/LinkingC2.v bound/LowerBoundExtra.v bound/LowerBound.v`)
 loc=loc-temp
-tex = tex + temp.to_s + " \\\\ \n"
+breakdown = breakdown + temp.to_s + " \\\\ \n"
 
 puts
 puts "<<WHOLE>>"
 puts
 print_result(`find . ! -path '*demo/*' -name '*.v' ! -name 'UpperBound_AExtra.v' ! -name 'UpperBound_A.v' ! -name 'UpperBound_B.v' | xargs coqwc`)
 
-tex.sub! 'FILL_THIS', loc.to_s
+breakdown.sub! 'FILL_THIS', loc.to_s
+puts "----------------------"
+print tex1
+puts
+print tex2
+puts
+print tex3
 puts
 puts "----------------------"
-puts
-print tex
+print breakdown
+

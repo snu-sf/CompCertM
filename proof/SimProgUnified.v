317b07b1e61bd94bcddd9205890d0bb2e9eaf18e
alxest <youngju.song@sf.snu.ac.kr>
2020-03-12 19:25:20 +0900

    WANT: unify Unified.ProgPair.t & ProgPair.t.

--- proof/SimProgUnified.v
+++ proof/SimProgUnified.v
@@ -47,65 +47,44 @@ Hint Unfold ProgPair.sim ProgPair.src ProgPair.tgt.
 (* Hint Unfold ProgPair.ss_link. *)
 
 
+Require Import SimProg.
+Require Import SimMod.
 
+Axiom mp_to_mp: forall `{SimMem.class} `{SimSymb.class},
+    SimMod.ModPair.t -> SimModUnified.ModPair.t.
 
+Theorem unification
+        `{SimMem.class}
+        `{Sound.class}
+        `{SimSymb.class}
+        pp
+        (SIM: ProgPair.sim pp)
+  :
+    exists SMOS ppu,
+      (<<SIM: SimProgUnified.ProgPair.sim ppu (SMOS := SMOS)>>)
+      /\ (<<PROGSRC: (SimProgUnified.ProgPair.src ppu) = (ProgPair.src pp)>>)
+      /\ (<<PROGTGT: (SimProgUnified.ProgPair.tgt ppu) = (ProgPair.tgt pp)>>)
+.
+Proof.
+  ginduction pp; ii; ss.
+  { unshelve esplits; eauto. admit "Make default instance for SimMemOhs". }
+  inv SIM.
+  exploit IHpp; eauto. i; des.
+  assert(exists SMOS',
+            (<<SIM: SimProgUnified.ProgPair.sim (SMOS := SMOS') ppu>>) /\
+            (<<SIM: SimModUnified.ModPair.sim (SMOS := SMOS') (mp_to_mp a)>>)
+        ).
+  { admit "merge theorem". }
+  des.
+  eexists SMOS', ((mp_to_mp a) :: ppu).
+  esplits; ss; eauto.
+Qed.
 
+SimProg.ProgPair.t
+SimProgUnified.ProgPair.t
 
-Section SIM.
-Context `{SMOS: SimMemOhs.class} {SS: SimSymb.class SM} {SU: Sound.class}.
+SimMod.ModPair.t
+SimModUnified.ModPair.t
 
-  Variable pp: ProgPair.t.
-  Hypothesis SIMPROG: ProgPair.sim pp.
-  Let p_src := (ProgPair.src pp).
-  Let p_tgt := (ProgPair.tgt pp).
-
-
-
-  Theorem sim_link_sk
-          sk_link_src
-          (LOADSRC: (link_sk p_src) = Some sk_link_src)
-          (WF: forall md, In md p_src -> <<WF: Sk.wf md>>):
-      exists ss_link sk_link_tgt,
-        <<LOADTGT: (link_sk p_tgt) = Some sk_link_tgt>>
-        /\ <<SIMSK: SimSymb.wf ss_link>>
-        /\ <<SKSRC: ss_link.(SimSymb.src) = sk_link_src>>
-        /\ <<SKTGT: ss_link.(SimSymb.tgt) = sk_link_tgt>>
-        /\ <<LE: Forall (fun mp => (SimSymb.le mp.(ModPair.ss) ss_link)) pp>>.
-  Proof.
-    u. subst_locals. ginduction pp; ii; ss. destruct a; ss.
-    unfold ProgPair.src in *. unfold link_sk in *. ss. destruct (classic (t = [])).
-    { clarify; ss. cbn in *. clarify. clear IHt. inv SIMPROG. inv H2. inv H1. ss.
-      esplits; eauto. econs; eauto. ss. refl.
-    }
-    rename H into NNIL. eapply link_list_cons_inv in LOADSRC; cycle 1.
-    { destruct t; ss. }
-    des. rename sk_link_src into sk_link_link_src. rename restl into sk_link_src.
-    inv SIMPROG. exploit IHt; eauto. intro IH; des.
-    inv H1. ss. exploit (SimSymb.wf_link).
-    3: { rewrite SKSRC. eapply HD. }
-    all: eauto.
-    { rewrite SKSRC. eapply WF; et. }
-    { eapply link_list_preserves_wf_sk; eauto. }
-    { eapply SimSymb.wf_preserves_wf; et. rewrite SKSRC. eapply WF; et. }
-    { eapply SimSymb.wf_preserves_wf; et. eapply link_list_preserves_wf_sk; eauto. }
-    i; des. esplits; eauto.
-    - eapply link_list_cons; eauto.
-      rewrite SKTGT in LINKTGT. ss.
-    - econs; eauto. rewrite Forall_forall in *. ii.
-      all ltac:(fun H => apply link_list_linkorder in H). des.
-      rewrite Forall_forall in *. etrans; eauto.
-  Qed.
-
-End SIM.
-
-
-
-
-
-
-
-
-
-
-
-
+SimModSem.ModSemPair.t
+SimModSemUnified.ModSemPair.t

7db402677adaf2da959d52358b2a328f137f5933
alxest <youngju.song@sf.snu.ac.kr>
2020-05-25 14:26:38 +0900

    WIP -- need to use "Polymorphic" in step anyway. Can we avoid it?

--- compose/ModTuple.v
+++ compose/ModTuple.v
@@ -30,35 +30,35 @@ End CHECK.
 
 
 
-(* Section ANY. *)
+Section ANY.
 
-(* (* Variable T : Type@{i}. *) *)
-(* Polymorphic Inductive Any: Type := *)
-(*   Any_intro : forall {A:Type} {x:A}, Any. *)
+(* Variable T : Type@{i}. *)
+Polymorphic Inductive Any: Type :=
+  Any_intro : forall {A:Type} {x:A}, Any.
 
-(* (* Arguments Any [A P]. *) *)
+(* Arguments Any [A P]. *)
 
-(* Section UNIV. *)
+Section UNIV.
   
-(* Polymorphic Universe i j. *)
-(* Polymorphic Definition downcast (T: Type@{j}) (a: Any@{i}): option T. *)
-(* (* Polymorphic Definition downcast (a: Any) (T: Type): option T. *) *)
-(* destruct a. *)
-(* destruct (ClassicalDescription.excluded_middle_informative (A = T)). *)
-(* - subst. apply Some. assumption. *)
-(* - apply None. *)
-(* Defined. *)
+Polymorphic Universe i j.
+Polymorphic Definition downcast (T: Type@{j}) (a: Any@{i}): option T.
+(* Polymorphic Definition downcast (a: Any) (T: Type): option T. *)
+destruct a.
+destruct (ClassicalDescription.excluded_middle_informative (A = T)).
+- subst. apply Some. assumption.
+- apply None.
+Defined.
 
-(* End UNIV. *)
+End UNIV.
 
-(* Polymorphic Definition upcast {T} (a: T): Any := @Any_intro _ a. *)
+Polymorphic Definition upcast {T} (a: T): Any := @Any_intro _ a.
 
-(* End ANY. *)
+End ANY.
 
-(* Arguments Any_intro {A} x. *)
-(* Print downcast. *)
-(* Print upcast. *)
-(* Print Any. *)
+Arguments Any_intro {A} x.
+Print downcast.
+Print upcast.
+Print Any.
 
 
 
@@ -186,13 +186,13 @@ Module ModSem2.
   (* } *)
   (* . *)
 
-  (* Definition owned_heap: Type := alist (fun n => match nth_error mss n with *)
-  (*                                                | Some ms => ms.(ModSem.owned_heap) *)
-  (*                                                | _ => Empty_set *)
-  (*                                                end) *)
-  (* . *)
+  Definition owned_heap: Type := alist (fun n => match nth_error mss n with
+                                                 | Some ms => ms.(ModSem.owned_heap)
+                                                 | _ => Empty_set
+                                                 end)
+  .
 
-  Definition owned_heap: Type := Midx.t -> Any.
+  (* Definition owned_heap: Type := Midx.t -> Any. *)
 
   Definition genvtype: Type := alist (fun n => match nth_error mss n with
                                                | Some ms => ms.(ModSem.genvtype)
@@ -256,7 +256,7 @@ Module ModSem2.
 
       (AT: ms.(ModSem.at_external) hd oh args)
       (MSFIND: find_fptr_owner (Args.get_fptr args) ms_new)
-      (OHS: ohs1 = Midx.update ohs0 ms.(ModSem.midx) (upcast oh))
+      (* (OHS: ohs1 = Midx.update ohs0 ms.(ModSem.midx) (upcast oh)) *)
       WTY0 NNIL0 WTY1 NNIL1
     :
       step se ge (State (upcast (n, hd) :: tl) ohs0 WTY0 NNIL0)
@@ -302,9 +302,73 @@ Module ModSem2.
   Variable link_skenv: SkEnv.t -> SkEnv.t -> SkEnv.t.
 
   Set Printing Universes.
+
+  Record state2: Type := mkState {
+    stk: list Any;
+    ohs: owned_heap;
+    is_call: bool;
+    WTY: forall
+        any
+        (IN: In any stk)
+      ,
+        exists n ms st, (<<NTH: nth_error mss n = Some ms>>) /\
+                        (<<CAST: @downcast (nat * ms.(ModSem.state)) any = Some (n, st)>>);
+    NNIL: stk <> nil;
+  }
+  .
+  Arguments mkState stk: clear implicits.
+
+  Inductive step2 (se: Senv.t) (ge: genvtype): state2 -> trace -> state2 -> Prop :=
+  | step2_call_inside
+      n ms ms_new (hd: ms.(ModSem.state)) tl tr ohs0 ohs1 args oh
+      (NTH: nth_error mss n = Some ms)
+
+      (AT: ms.(ModSem.at_external) hd oh args)
+      (MSFIND: find_fptr_owner (Args.get_fptr args) ms_new)
+      (* (OHS: ohs1 = Midx.update ohs0 ms.(ModSem.midx) (upcast oh)) *)
+      WTY0 NNIL0 WTY1 NNIL1
+    :
+      step2 se ge (mkState (upcast (n, hd) :: tl) ohs0 false WTY0 NNIL0)
+           tr (mkState (upcast (n, hd) :: tl) ohs1 true WTY1 NNIL1)
+  (* | step_call_inside *)
+  (*     fr0 frs args oh ohs0 ohs1 ms *)
+  (*     (AT: fr0.(Frame.ms).(ModSem.at_external) fr0.(Frame.st) oh args) *)
+  (*     (MSFIND: find_fptr_owner (Args.get_fptr args) ms) *)
+  (*     (OHS: ohs1 = Midx.update ohs0 fr0.(Frame.ms).(ModSem.midx) (upcast oh)): *)
+  (*     step se ge (State (fr0 :: frs) ohs0) *)
+  (*          E0 (Callstate args (fr0 :: frs) ohs1) *)
+
+  (* | step_init_inside *)
+  (*     args frs ms st_init oh ohs *)
+  (*     (MSFIND: find_fptr_owner (Args.get_fptr args) ms) *)
+  (*     (OH: Midx.get ohs ms.(ModSem.midx) = upcast oh) *)
+  (*     (INIT: ms.(ModSem.initial_frame) oh args st_init): *)
+  (*     step se ge (Callstate args frs ohs) *)
+  (*          E0 (State ((Frame.mk ms st_init) :: frs) ohs) *)
+
+  | step2_internal
+      n ms (hd0 hd1: ms.(ModSem.state)) tl tr ohs
+      (NTH: nth_error mss n = Some ms)
+
+      (STEP: Step ms hd0 tr hd1)
+      WTY0 NNIL0 WTY1 NNIL1
+    :
+      step2 se ge (mkState (upcast (n, hd0) :: tl) ohs false WTY0 NNIL0)
+           tr (mkState (upcast (n, hd1) :: tl) ohs false WTY1 NNIL1)
+
+  (* | step_return *)
+  (*     fr0 fr1 frs retv st0 ohs0 ohs1 oh0 oh1 *)
+  (*     (FINAL: fr0.(Frame.ms).(ModSem.final_frame) fr0.(Frame.st) oh0 retv) *)
+  (*     (AFTER: fr1.(Frame.ms).(ModSem.after_external) fr1.(Frame.st) oh1 retv st0) *)
+  (*     (OHS: ohs1 = Midx.update ohs0 fr0.(Frame.ms).(ModSem.midx) (upcast oh0)) *)
+  (*     (OH: Midx.get ohs1 fr1.(Frame.ms).(ModSem.midx) = upcast oh1): *)
+  (*     step se ge (State (fr0 :: fr1 :: frs) ohs0) *)
+  (*          E0 (State (((Frame.update_st fr1) st0) :: frs) ohs1) *)
+  .
+
   Program Definition t': ModSem.t :=
     {|
-      (* ModSem.state := state; *)
+      ModSem.state := state2;
       ModSem.owned_heap := owned_heap;
       ModSem.genvtype := genvtype;
       (* ModSem.step := step; *)

2791f26c82b52501c07b46ce008b952371a52bb8
alxest <youngju.song@sf.snu.ac.kr>
2020-06-03 01:42:16 +0900

    Tweak

--- specIR/SIR.v
+++ specIR/SIR.v
@@ -25,25 +25,40 @@ From ITree Require Import
 
 Import SumNotations.
 
+Require Import MapsC.
 Require Import ValuesC.
 Require Import MemoryC.
 Require Import CoqlibC.
 Require Import ASTC.
 Require Import EventsC.
 Require Import GlobalenvsC.
-Require Import Mod ModSem.
+Require Import Mod ModSem Any.
+
+Import ITreeNotations.
+Import Monads.
+Import MonadNotation.
 
 
 
 
 Section EFF.
-  Definition var : Set := positive.
+  Definition var: Set := positive.
 
-  Variant LocalE : Type -> Type :=
+  Variant LocalE: Type -> Type :=
   | LGet (x: var) : LocalE val
   | LSet (x: var) (v: val) : LocalE unit
   | LPush: LocalE unit
   | LPop: LocalE unit
+  .
+
+  Variant GlobalE: Type -> Type :=
+  | GGet (x: var) : GlobalE val
+  | GSet (x: var) (v: val) : GlobalE unit
+  .
+
+  Variant OwnedHeapE: Type -> Type :=
+  | OGet: OwnedHeapE Any
+  | OSet (a: Any) : OwnedHeapE unit
   .
 
   Variant MemE: Type -> Type :=
@@ -53,18 +68,42 @@ Section EFF.
   | MFree (blk: block) (lo hi: Z): MemE unit
   .
 
-  Variant CallE: Type -> Type :=
-  | CCall (fptr: val) (args: list val): CallE val
+  Variant InternalCallE: Type -> Type :=
+  | ICall (name: ident) (args: list val): InternalCallE val
+  .
+
+  Variant ExternalCallE: Type -> Type :=
+  | ECall (fptr: val) (args: list val): ExternalCallE val
   .
 
   Variant EventE: Type -> Type :=
-  | ENB: EventE void
-  | EUB: EventE void
+  | ENB (msg: string): EventE void
+  | EUB (msg: string): EventE void
   | ESyscall (ef: external_function) (args: list val): EventE val
   .
 
-  Definition eff: Type -> Type := LocalE +' MemE +' CallE +' EventE.
+  Definition eff0: Type -> Type := ExternalCallE +' LocalE +' GlobalE +' OwnedHeapE +' MemE +' EventE.
+  Definition eff: Type -> Type := InternalCallE +' eff0.
 End EFF.
+
+Definition triggerUB {E A} `{EventE -< E} (msg: string): itree E A :=
+  vis (EUB msg) (fun v => match v: void with end)
+.
+Definition triggerNB {E A} `{EventE -< E} (msg: string) : itree E A :=
+  vis (ENB msg) (fun v => match v: void with end)
+.
+
+Definition unwrapN {E X} `{EventE -< E} (x: option X): itree E X :=
+  match x with
+  | Some x => ret x
+  | None => triggerNB "unwrap"
+  end.
+
+Definition unwrapU {E X} `{EventE -< E} (x: option X): itree E X :=
+  match x with
+  | Some x => ret x
+  | None => triggerUB "unwrap"
+  end.
 
 
 
@@ -72,27 +111,84 @@ End EFF.
 
 Local Open Scope sum_scope.
 
+(* Definition itr: Type := itree eff val. *)
+(* Definition ktr: Type := ktree eff block val. *)
+
+Definition program: Type := AST.program (InternalCallE ~> itree eff) unit.
+
+Variable p: program.
+Variable func_name: ident.
+Check ((prog_defmap p) ! func_name).
+
+(* Section SCOPE. *)
+(*   Context {eff : Type -> Type}. *)
+(*   Context {HasInternalCall: InternalCallE -< eff}. *)
+(*   Context {HasExternalCall: ExternalCallE -< eff}. *)
+(*   Context {HasLocal: LocalE -< eff}. *)
+(*   Context {HasMem: MemE -< eff}. *)
+(*   Context {HasEvent: EventE -< eff}. *)
+(* End SCOPE. *)
+Section TMP.
+  Variable A: Type -> Type.
+  Variable B: Type -> Type.
+  Variable C: Type -> Type.
+  Variable D: Type -> Type.
+  Variable E: Type -> Type.
+  Let eff: Type -> Type := A +' B +' C +' D +' E.
+  Check ((A +' B) +' (C +' D)) +' E.
+  Variable a: A unit.
+  Variable c: C unit.
+  Check (a).
+  Check (|a).
+  Check (||a).
+  Check (|||a).
+  Check (|||||||a).
+  Check (a|).
+  (* Check (a||). *)
+  Check (|a|).
+  Check (||a|).
+  Check (||||||a|).
+  (* Check (||||a||). *)
+  Check (trigger c: itree eff unit).
+End TMP.
+
+Definition denote_function (p: program): (InternalCallE ~> itree eff) :=
+  fun T ei =>
+    let '(ICall func_name args) := ei in
+    match ((prog_defmap p) ! func_name) with
+    | Some (Gfun f) =>
+      trigger LPush ;;
+      retv <- f _ (ICall 1%positive args) ;;
+      trigger LPop ;;
+      Ret retv
+    | _ => triggerNB "TODO: UB? NB? Which one is useful?"
+    end
+.
+
+Definition denote_program (p: program): (InternalCallE ~> itree eff0) := mrec (denote_function p).
+
+
+
 Section MODSEM.
+
+  Variable p: program.
   (* Context {eff : Type -> Type}. *)
   (* Context {HasLocalE: LocalE -< eff}. *)
   (* Context {HasMemE: MemE -< eff}. *)
   (* Context {HasCallE : CallE -< eff}. *)
   (* Context {HasEventE : EventE -< eff}. *)
 
-
   (* Variable itr: sir. *)
   (* Variable mi: Midx.t. *)
 
-  Definition itr := itree eff val.
-
   Record state := mk {
-    it: list itr;
+    it: itree eff0 val;
     lenv: list (var -> option val);
     m: mem;
   }
   .
 
-  Check (observe itr): (itreeF eff val (itree eff val)).
+  (* Check (observe itr): (itreeF eff val (itree eff val)). *)
 
   Inductive step (se: Senv.t) (ge: unit): state -> trace -> state -> Prop :=
   | step_tau

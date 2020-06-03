18bbd53b2eaf1f7628f26f1cef88e999bc0d55f9
alxest <youngju.song@sf.snu.ac.kr>
2020-06-03 21:43:33 +0900

    SIR compiles

--- specIR/SIR.v
+++ specIR/SIR.v
@@ -24,6 +24,9 @@ From ITree Require Import
 .
 
 Import SumNotations.
+Import ITreeNotations.
+Import Monads.
+Import MonadNotation.
 
 Require Import MapsC.
 Require Import ValuesC.
@@ -35,13 +38,14 @@ Require Import GlobalenvsC.
 Require Import IntegersC.
 Require Import Mod ModSem Any Skeleton.
 
-Import ITreeNotations.
-Import Monads.
-Import MonadNotation.
+Require Import Program.
+
 
 
 
 Set Implicit Arguments.
+
+
 
 
 (*** Put some other place ***)
@@ -114,60 +118,21 @@ Section EFF.
   | GFindS (x: ident) : GlobalE val
   .
 
-  (* Definition OwnedHeapE: Type -> Type := State.stateE Any *)
-  (* Definition OwnedHeapE: Type -> Type := State.stateE owned_heap. *)
-  Variant OwnedHeapE: Type -> Type :=
-  | OGet: OwnedHeapE owned_heap
-  | OPut (a: owned_heap): OwnedHeapE unit
-  .
-
-  (* Definition MemE: Type -> Type := State.stateE mem. *)
-  (* Variant MemE: Type -> Type := *)
-  (* | MGet: MemE mem *)
-  (* | MPut (m: mem): MemE unit *)
-  (* . *)
-
-  Section PLAYGROUND.
-    Check (Type -> Type).
-    (* Type@{Top.3861} -> Type@{Top.3862} *)
-    (*   : Type@{max(Top.3861+1,Top.3862+1)} *)
-
-    (*** as you can see (and stated in CPDT) (Type@{a} -> Type@{b})'s universe is simply max(a+1, b+1).
-         If we include "| MPut (m: mem): MemE unit" in MemE
-         --- it dosen't matter if we define it here, or by instantiating State.stateE mem ---
-         Its type becomes "Type@{MemE.u0} -> Type@{max(block.u0,PMap.t.u1)}"
-         --- "Pmap.t.u1" is Mem.mem's universe ---
-         So, MemE's universe becomes "+1" of it. Check (Type@{MemE.u0} -> Type@{max(block.u0,PMap.t.u1)}).
-         This means MemE has bigger universe that usual CompCert, so does my "program",
-         so I cannot use "prog_defmap" which expects usual CompCert-universe.
-     ***)
-  End PLAYGROUND.
-    
-  Variant MemE: Type -> Type :=
-  | MLoad (chunk: memory_chunk) (blk: block) (ofs: Z): MemE val
-  | MStore (chunk: memory_chunk) (blk: block) (ofs: Z) (v: val): MemE unit
-  | MAlloc (lo hi: Z): MemE block
-  | MFree (blk: block) (lo hi: Z): MemE unit
-  .
-
   Variant InternalCallE: Type -> Type :=
-  | ICall (name: ident) (args: list val): InternalCallE val
+  | ICall (oh: owned_heap) (name: ident) (vs: list val) (m: mem): InternalCallE (owned_heap * mem * val)
   .
 
   Variant ExternalCallE: Type -> Type :=
-  (* | ECall (fptr: val) (args: list val): ExternalCallE (owned_heap * val) *)
-  | ECall (fptr: val) (vs: list val) (m: mem): ExternalCallE (owned_heap * mem * val)
+  | ECall (oh: owned_heap) (fptr: val) (vs: list val) (m: mem): ExternalCallE (owned_heap * mem * val)
   .
 
   Variant EventE: Type -> Type :=
   | ENB (msg: string): EventE void
   | EUB (msg: string): EventE void
-  | ESyscall (ef: external_function) (args: list val): EventE val
+  | ESyscall (ef: external_function) (args: list val) (m0: mem): EventE (mem * val)
   .
 
-  Definition eff4: Type -> Type := Eval compute in ExternalCallE +' EventE.
-  Definition eff3: Type -> Type := Eval compute in MemE +' eff4.
-  Definition eff2: Type -> Type := Eval compute in OwnedHeapE +' eff3.
+  Definition eff2: Type -> Type := Eval compute in ExternalCallE +' EventE.
   Definition eff1: Type -> Type := Eval compute in GlobalE +' eff2.
   Definition eff0: Type -> Type := Eval compute in LocalE +' eff1.
   Definition eff: Type -> Type := Eval compute in InternalCallE +' eff0.
@@ -219,13 +184,13 @@ Section DENOTE.
 
   Definition denote_function: (InternalCallE ~> itree eff) :=
     fun T ei =>
-      let '(ICall func_name args) := ei in
+      let '(ICall oh func_name vs m) := ei in
       match (find (fun nf => ident_eq func_name (fst nf)) p.(prog_defs)) with
       | Some (_, Gfun (Internal f)) =>
       (* match (prog_defmap p) ! func_name with *)
       (* | Some (Gfun (Internal f)) => *)
         trigger LPush ;;
-                retv <- f.(fn_code) (ICall 1%positive args) ;;
+                retv <- f.(fn_code) ei ;;
                 trigger LPop ;;
                 Ret retv
       | _ => triggerNB "TODO: UB? NB? Which one is useful?"
@@ -261,44 +226,12 @@ Section DENOTE.
     interp (case_ (handle_GlobalE) (id_ _)) t
   .
 
-  Definition handle_OwnedHeapE {E: Type -> Type} `{EventE -< E}: OwnedHeapE ~> stateT owned_heap (itree E) :=
-    fun _ e oh0 =>
-      match e with
-      | OGet => Ret (oh0, oh0)
-      | OPut oh1 => Ret (oh1, tt)
-      end
-  .
-
-  Definition interp_OwnedHeapE {E A} `{EventE -< E} (t: itree (OwnedHeapE +' E) A): stateT owned_heap (itree E) A :=
-    let t' := State.interp_state (case_ handle_OwnedHeapE State.pure_state) t in
-    t'
-  .
-
-  Definition handle_MemE {E: Type -> Type} `{EventE -< E}: MemE ~> stateT mem (itree E) :=
-    fun _ e m0 =>
-      match e with
-      | MLoad chunk blk ofs => v <- unwrapU (Mem.load chunk m0 blk ofs) ;; Ret (m0, v)
-      | MStore chunk blk ofs v => m1 <- unwrapU (Mem.store chunk m0 blk ofs v) ;; Ret (m1, tt)
-      | MAlloc lo hi => let '(m1, blk) := Mem.alloc m0 lo hi in Ret (m1, blk)
-      | MFree blk lo hi => m1 <- unwrapU (Mem.free m0 blk lo hi) ;; Ret (m1, tt)
-      end
-  .
-
-  Definition interp_MemE {E A} `{EventE -< E} (t : itree (MemE +' E) A): stateT mem (itree E) A :=
-    let t' := State.interp_state (case_ handle_MemE State.pure_state) t in
-    t'
-  .
-
-  Definition denote_program (oh: owned_heap) (m0: mem):
-    (forall T, InternalCallE T -> itree eff4 (owned_heap * mem * T)) :=
+  Definition denote_program: (InternalCallE ~> itree eff2) :=
     let sem0: (InternalCallE ~> itree eff0) := mrec denote_function in
     fun _ ic =>
       let sem1: itree eff1 _ := ret <- (interp_LocalE (sem0 _ ic) []) ;; Ret (snd ret) in
       let sem2: itree eff2 _ := interp_GlobalE sem1 in
-      let sem3: itree eff3 _ := ret <- (interp_OwnedHeapE sem2 oh) ;; Ret ret in
-      let sem4: itree eff4 _ := ret <- (interp_MemE sem3 m0) ;;
-                                    Ret ((fst (snd ret)), (fst ret), (snd (snd ret))) in
-      sem4
+      sem2
   .
 
 End DENOTE.
@@ -307,29 +240,29 @@ End DENOTE.
 
 Section MODSEM.
 
+  Variable mi: string.
+  Variable initial_owned_heap: owned_heap.
   Variable skenv_link: SkEnv.t.
   Variable p: program.
   Let skenv: SkEnv.t := (SkEnv.project skenv_link) (Sk.of_program fn_sig p).
   (* Let ge: genv := (SkEnv.revive skenv) p. *)
-  Let ge: SkEnv.t := skenv.
+  Definition genvtype: Type := unit.
+  Let ge: genvtype := tt.
 
-  Definition state := itree eff4 (owned_heap * mem * val).
-  (* Record state := mk { *)
-  (*   it: itree eff4 val; *)
-  (* } *)
-  (* . *)
+  Definition state := itree eff2 (owned_heap * mem * val).
+
   Let denote_program := denote_program p skenv.
 
-  Inductive initial_frame (oh: owned_heap) (args: Args.t): state -> Prop :=
+  Inductive initial_frame (oh0: owned_heap) (args: Args.t): state -> Prop :=
   | initial_frame_intro
       fid blk m0 vs itr
       (SYMB: Genv.find_symbol skenv fid = Some blk)
       (FPTR: (Args.fptr args) = Vptr blk Ptrofs.zero)
       (VS: (Args.vs args) = vs)
       (M: (Args.m args) = m0)
-      (DENOTE: denote_program oh m0 (ICall fid vs) = itr)
+      (DENOTE: denote_program (ICall oh0 fid vs m0) = itr)
     :
-      initial_frame oh args itr
+      initial_frame oh0 args itr
   .
 
   Goal forall (st0 st1: state), st0 = st1 -> False.
@@ -343,94 +276,99 @@ Section MODSEM.
   Inductive at_external (st0: state): owned_heap -> Args.t -> Prop :=
   | at_external_intro
       oh0 m0 args fptr vs k
-      (*** TODO: oh0 is free ***)
-      (VIS: (observe st0) = VisF (inl1 (ECall fptr vs m0)) k)
-      (* (VIS: (observe st0) = VisF (subevent _ (ECall fptr vs)) k) *)
+      (VIS: (observe st0) = VisF (subevent _ (ECall oh0 fptr vs m0)) k)
       (ARGS: args = Args.mk fptr vs m0)
     :
       at_external st0 oh0 args
   .
 
-  Inductive after_external (st0: state): owned_heap -> Retv.t -> state -> Prop :=
+  Inductive get_k (st0: state): (owned_heap * mem * val -> itree eff2 (owned_heap * mem * val)) -> Prop :=
+  | get_k_intro
+      _oh0 _m0 _fptr _vs k
+      (VIS: (observe st0) = VisF (subevent _ (ECall _oh0 _fptr _vs _m0)) k)
+    :
+      get_k st0 k
+  .
+
+  Inductive after_external (st0: state) (oh0: owned_heap) (retv: Retv.t): state -> Prop :=
   | after_external_intro
-      m0 fptr vs k 
-      (VIS: (observe st0) = VisF (inl1 (ECall fptr vs)) k)
-      m0 v0
-      (RETV: retv = Retv.mk m0 v0)
-      (ST: st1 = k v0)
+      k m0 rv st1
+      (GETK: get_k st0 k)
+      (V: (Retv.v retv) = rv)
+      (M: (Retv.m retv) = m0)
+      (KONT: st1 = k (oh0, m0, rv))
     :
       after_external st0 oh0 retv st1
   .
 
   Inductive final_frame (st0: state): owned_heap -> Retv.t -> Prop :=
   | final_frame_intro
-      oh0 m0 v0
-      (RET: (observe st0) = RetF (oh0, m0, v0))
+      oh0 m0 rv
+      (RET: (observe st0) = RetF (oh0, m0, rv))
     :
-      final_frame st0 oh0 (Retv.mk v0 m0)
+      final_frame st0 oh0 (Retv.mk rv m0)
   .
 
-  Inductive step (se: Senv.t) (ge: unit): state -> trace -> state -> Prop :=
+  Inductive step (se: Senv.t) (ge: genvtype) (st0: state): trace -> state -> Prop :=
   | step_tau
-      st0 E0 st1
+      E0 st1
       (TAU: (observe st0) = TauF st1)
     :
       step se ge st0 E0 st1
+  (*** ub is stuck, so we don't state anything ***)
+  | step_nb
+      msg k
+      (VIS: (observe st0) = VisF (subevent _ (ENB msg)) k)
+    :
+      step se ge st0 E0 st0
+  | step_syscall
+      ef vs m0 tr rv m1 k st1
+      (VIS: (observe st0) = VisF (subevent _ (ESyscall ef vs m0)) k)
+      (SYSCALL: external_call ef se vs m0 tr rv m1)
+      (KONT: st1 = k (m1, rv))
+    :
+      step se ge st0 tr st1
   .
 
   Program Definition modsem: ModSem.t :=
-    {| ModSem.step := step1;
+    {| ModSem.step := step;
+       ModSem.owned_heap := owned_heap;
        ModSem.at_external := at_external;
        ModSem.initial_frame := initial_frame;
        ModSem.final_frame := final_frame;
        ModSem.after_external := after_external;
+       ModSem.initial_owned_heap := initial_owned_heap;
        ModSem.globalenv := ge;
        ModSem.skenv := skenv;
        ModSem.skenv_link := skenv_link;
-       ModSem.midx := None;
+       ModSem.midx := Some mi;
     |}.
+  Next Obligation.
+    inv AT0. inv AT1. rewrite VIS in *. clarify.
+  Qed.
+  Next Obligation.
+    inv FINAL0. inv FINAL1. rewrite RET in *. clarify.
+  Qed.
+  Next Obligation.
+    inv AFTER0. inv AFTER1. inv GETK. inv GETK0. rewrite VIS in *.
+    ss. clarify. simpl_depind. clarify.
+  Qed.
+  Next Obligation.
+    ii. des. inv PR; ss. inv PR0; try rewrite VIS in *; ss; clarify.
+  Qed.
+  Next Obligation.
+    ii. des. inv PR0; ss. inv PR; try rewrite RET in *; ss; clarify.
+  Qed.
+  Next Obligation.
+    ii. des. inv PR; ss. inv PR0; try rewrite VIS in *; ss; clarify.
+  Qed.
 
 End MODSEM.
-Definition module (itr: itree eff val) (mi: Midx.t): Mod.t.
 
-
-
-Section REMOVELATER.
-Inductive expr : Type :=
-| Var (_ : var)
-| Val (_ : val)
-(* | Load (_: var) (_: expr) *)
-(* | CoqCode (_: list (var + expr)) (P: list val -> (val * list val)) *)
-| Shallow (itr: itree eff val)
-(* | Syscall (code: string) (msg: string) (e: expr) *)
-(* | Call (func_name: string) (params: list (var + expr)) *)
-(* | PutOwnedHeap (_: expr) *)
-(* | GetOwnedHeap *)
+Program Definition module (p: program) (mi: string) (initial_owned_heap: owned_heap): Mod.t :=
+  {| Mod.data := p; Mod.get_sk := (Sk.of_program fn_sig); Mod.get_modsem := modsem mi initial_owned_heap;
+     Mod.midx := Some mi |}
 .
 
-Inductive stmt : Type :=
-| Assign (x : var) (e : expr)    (* x = e *)
-| Seq    (a b : stmt)            (* a ; b *)
-| If     (i : expr) (t e : stmt) (* if (i) then { t } else { e } *)
-| While  (t : expr) (b : stmt)   (* while (t) { b } *)
-| Skip                           (* ; *)
-| AssumeFail
-| GuaranteeFail
-| Store (x: var) (ofs: expr) (e: expr) (* x->ofs := e *)
-| Expr (e: expr)
-| Return (e: expr)
-| Break
-| Continue
-.
+End OWNEDHEAP.
 
-Section Denote.
-
-  Fixpoint denote_expr (e : expr) : itree eff val :=
-    match e with
-    | Var v     => trigger (LGet v)
-    | Val n     => ret n
-    | Shallow itr => itr
-    end.
-
-End Denote.
-End REMOVELATER.

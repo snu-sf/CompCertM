cba5d35460873136bfa72123298ebbb9137c24e1
alxest <youngju.song@sf.snu.ac.kr>
2020-06-19 14:25:58 +0900

    WIP - maybe try equality first. not sure if SM/SV prmtrztion is useful

--- specIR/SimSIR.v
+++ specIR/SimSIR.v
@@ -29,25 +29,130 @@ Local Obligation Tactic := ii; ss; eauto.
 
 Section OWNEDHEAP.
 
-Context `{SimMem.class}.
-
 Variable mi: string.
 Variable owned_heap0 owned_heap1: Type.
-Variable OR: owned_heap0 -> owned_heap1 -> Prop.
+Variable SO: owned_heap0 -> owned_heap1 -> Prop.
+Variable SM: mem -> mem -> Prop.
+Variable SV: val -> val -> Prop.
+Hypothesis (SVINT: forall v_src v_tgt i, v_src = Vint i -> SV v_src v_tgt -> v_tgt = Vint i).
+
 
 Let ktr0 := function owned_heap0.
 Let ktr1 := function owned_heap1.
 Let itr0 := itree (E owned_heap0) (owned_heap0 * (mem * val)).
 Let itr1 := itree (E owned_heap1) (owned_heap1 * (mem * val)).
 
-Inductive sim_itr (sim_itr: itr0 -> itr1 -> Prop): itr0 -> itr1 -> Prop :=
+(**************** TODO: eqit is defined in terms of "itree'", not "itree" *****************)
+
+
+(* Variable fname: ident. *)
+(* Variable oh0: owned_heap0. *)
+(* Variable m0: mem. *)
+(* Variable vs0: list val. *)
+(* Check (ICall fname oh0 m0 vs0). *)
+(* Variable k0: ((owned_heap0 * (mem * val)) -> itr0). *)
+(* Check (Vis (subevent _ (ICall fname oh0 m0 vs0)) k0). *)
+
+
+Inductive _sim_itr (sim_itr: itr0 -> itr1 -> Prop): itr0 -> itr1 -> Prop :=
+| sim_ret
+    oh0 m0 v0
+    oh1 m1 v1
+  :
+    _sim_itr sim_itr (Ret (oh0, (m0, v0))) (Ret (oh1, (m1, v1)))
+| sim_tau
+    i0
+    i1
+    (SIM: sim_itr i0 i1)
+  :
+    _sim_itr sim_itr (Tau i0) (Tau i1)
+| sim_icall
+    fname
+    oh0 m0 vs0 k0
+    oh1 m1 vs1 k1
+    (SIM: forall
+        ohr0 mr0 vr0
+        ohr1 mr1 vr1
+      ,
+        sim_itr (k0 (ohr0, (mr0, vr0))) (k1 (ohr1, (mr1, vr1))))
+  :
+    _sim_itr sim_itr
+             (Vis (subevent _ (ICall fname oh0 m0 vs0)) k0)
+             (Vis (subevent _ (ICall fname oh1 m1 vs1)) k1)
+| sim_ecall
+    sg
+    fptr0 oh0 m0 vs0 k0
+    fptr1 oh1 m1 vs1 k1
+    (SIM: forall
+        ohr0 mr0 vr0
+        ohr1 mr1 vr1
+      ,
+        sim_itr (k0 (ohr0, (mr0, vr0))) (k1 (ohr1, (mr1, vr1))))
+  :
+    _sim_itr sim_itr
+             (Vis (subevent _ (ECall sg fptr0 oh0 m0 vs0)) k0)
+             (Vis (subevent _ (ECall sg fptr1 oh1 m1 vs1)) k1)
+| sim_choose_both
+    X0 X1
+    k0 k1
+    (SIM: forall x1, exists x0, sim_itr (k0 x0) (k1 x1))
+  :
+    _sim_itr sim_itr
+             (Vis (subevent _ (EChoose X0)) k0)
+             (Vis (subevent _ (EChoose X1)) k1)
+| sim_choose_src
+    X0 X1
+    k0 k1
+    (SIM: forall x1, exists x0, sim_itr (k0 x0) (k1 x1))
+  :
+    _sim_itr sim_itr
+             (Vis (subevent _ (EChoose X0)) k0)
+             Tau 
 .
 
-(* Definition sim_ktr (k0: ktr0) (k1: ktr1): Prop := forall *)
-(*     oh0 oh1 m0 m1 vs0 vs1 *)
-(*     (SIMMEM: ) *)
-(*     . *)
+EventE
 
+Definition sim_itr: itr0 -> itr1 -> Prop := paco2 _sim_itr bot2.
+
+Lemma sim_itr_mon: monotone2 _sim_itr.
+Proof.
+Qed.
+Hint Unfold lxsim.
+Hint Resolve lxsim_mon: paco.
+
+Definition sim_ktr (k0: ktr0) (k1: ktr1): Prop := forall
+    oh0 oh1 m0 m1 vs0 vs1
+    (O: SO oh0 oh1)
+    (M: SM m0 m1)
+    (V: Forall2 SV vs0 vs1)
+  ,
+    sim_itr (k0 oh0 m0 vs0) (k1 oh1 m1 vs1)
+.
+
+
+
+
+(*** Lifting to SimModSem ***)
+
+Record t' := mk' {
+  src: mem;
+  tgt: mem;
+}.
+
+Program Instance SimMem: (SimMem.class) :=
+{ t := t';
+  src := src;
+  tgt := tgt;
+  ptt_src := fun _ _ _ => etc;
+  ptt_tgt := fun _ _ _ => etc;
+  wf := fun (sm0: t') => SM sm0.(src) sm0.(tgt);
+  le := top2;
+  lepriv := top2;
+  sim_val := fun _ => SV;
+  sim_val_list := fun _ => Forall2 SV;
+  unchanged_on := top3;
+}
+.
 
 Record t := mk {
   sm:> SimMem.t;
@@ -56,10 +161,10 @@ Record t := mk {
 }
 .
 
-Let wf (sm0:t): Prop := (SimMem.wf sm0) /\ (OR sm0.(oh_src) sm0.(oh_tgt)).
+Let wf (sm0:t): Prop := (SimMem.wf sm0) /\ (SO sm0.(oh_src) sm0.(oh_tgt)).
 Let set_sm (smo0:t) (sm0:SimMem.t): t := mk sm0 smo0.(oh_src) smo0.(oh_tgt).
 
-Program Definition SMO: (SimMemOh.class) :=
+Program Instance SimMemOh: (SimMemOh.class) :=
   {|
     SimMemOh.t := t;
     SimMemOh.sm := sm;
@@ -73,16 +178,6 @@ Program Definition SMO: (SimMemOh.class) :=
   |}
 .
 Next Obligation.
-  econs; ii.
-  - refl.
-  - etrans; et.
-Qed.
-Next Obligation.
-  econs; ii.
-  - refl.
-  - etrans; et.
-Qed.
-Next Obligation.
   rr in PR. des; ss.
 Qed.
 Next Obligation.
@@ -92,7 +187,54 @@ Next Obligation.
   destruct smo0; ss.
 Qed.
 
-
-
 End OWNEDHEAP.
 
+
+
+
+
+
+
+
+
+
+
+
+
+(*** Relating with SimMem Id ***)
+
+Program Definition boo := SimMem eq eq _.
+Next Obligation.
+  subst. ss.
+Qed.
+
+Require Import SimMemId.
+Lemma SimMem_eta
+      (smc0 smc1: SimMem.class)
+      (EQ0: smc0.(@SimMem.t) = smc1.(@SimMem.t))
+      (EQ1: smc0.(@SimMem.src) ~= smc1.(@SimMem.src))
+      (EQ2: smc0.(@SimMem.ptt_src) ~= smc1.(@SimMem.ptt_src))
+      (EQ3: smc0.(@SimMem.tgt) ~= smc1.(@SimMem.tgt))
+      (EQ4: smc0.(@SimMem.ptt_tgt) ~= smc1.(@SimMem.ptt_tgt))
+      (EQ5: smc0.(@SimMem.wf) ~= smc1.(@SimMem.wf))
+      (EQ6: smc0.(@SimMem.le) ~= smc1.(@SimMem.le))
+      (EQ7: smc0.(@SimMem.lepriv) ~= smc1.(@SimMem.lepriv))
+      (EQ8: smc0.(@SimMem.sim_val) ~= smc1.(@SimMem.sim_val))
+      (EQ9: smc0.(@SimMem.sim_val_list) ~= smc1.(@SimMem.sim_val_list))
+      (EQ10: smc0.(@SimMem.unchanged_on) ~= smc1.(@SimMem.unchanged_on))
+  :
+    smc0 = smc1
+.
+Proof.
+  destruct smc0, smc1; ss.
+  clarify.
+  apply_all_once JMeq_eq. clarify.
+  f_equal; try eapply Axioms.proof_irr.
+Qed.
+
+Goal boo = SimMemId.
+Proof.
+  eapply SimMem_eta; ss.
+Abort.
+  
+(*** Maybe we should define common ancestor, and let SimMemId // SimSIR use the same thing. ***)

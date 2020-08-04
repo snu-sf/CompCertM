4aa2f8a1f6d9f92e8428c2ed93ad6b91d8e8076f
alxest <youngju.song@sf.snu.ac.kr>
2020-08-04 14:30:58 +0900

    WIP

--- demo/fib/FibS01proof.v
+++ demo/fib/FibS01proof.v
@@ -90,6 +90,28 @@ Proof.
   exploit H0; et. i. eauto with paco.
 Qed.
 
+Definition rclo: (state owned_heap) -> (state owned_heap) -> Prop :=
+  rtc ((sim_st 0%nat) \2/ eutt eq).
+
+Global Instance rclo_bind: Proper ((eq ==> rclo) ==> rclo ==> rclo) (ITree.bind').
+Proof.
+  ii.
+  rename x0 into i0. rename y0 into i1.
+  rename x into k0. rename y into k1.
+  assert(LEMMA: forall x (k0 k1: owned_heap * (mem * val) -> state owned_heap),
+            ((eq ==> rclo) k0 k1) -> rclo (` x : _ <- x;; k0 x) (` x : _ <- x;; k1 x)).
+  { admit "". }
+  move LEMMA at top. move H0 at top.
+  revert_until LEMMA.
+  induction H0; ii; ss; et.
+  fold rclo in H0.
+  etrans; cycle 1.
+  { eapply IHclos_refl_trans_1n; eauto. }
+  clears z. clear z. clears k1. clear k1.
+Abort.
+
+
+
 Ltac my_tac := 
   repeat match goal with
          | [ |- context[interp_function ?H] ] =>
@@ -195,17 +217,19 @@ Proof.
   assert(V: to_nat_opt (of_nat (S n)) = Some (S n)).
   { admit "should be derived from guarantee". }
   des_ifs. unfold guarantee. rewrite V. ss. des_ifs.
-  setoid_rewrite <- H; cycle 1.
+
+
+  unfold f_fib. unfold unwrapU. rewrite unfold_interp_mrec. ss. des_ifs. rewrite ! bind_ret_l. ss.
+  rewrite <- H; cycle 1.
   { instantiate (1:= n). eauto. }
   { ss. }
-  unfold f_fib. unfold unwrapU. rewrite unfold_interp_mrec. ss. des_ifs.
-  rewrite ! bind_ret_l. ss.
-  rewrite unfold_interp_mrec. ss. rewrite ! bind_ret_l.
+  unfold f_fib. unfold unwrapU. rewrite unfold_interp_mrec. ss. des_ifs. repeat (rewrite bind_ret_l; ss).
   rewrite <- H; cycle 1.
   { instantiate (1:= S n). eauto. }
   { ss. }
-  unfold f_fib. ss. rewrite V. ss. rewrite bind_ret_l.
-  rewrite unfold_interp_mrec. ss. rewrite bind_ret_l. refl.
+  unfold f_fib. ss. unfold unwrapU. des_ifs. ss. rewrite bind_ret_l.
+  rewrite unfold_interp_mrec. ss. rewrite bind_ret_l.
+  refl.
 Qed.
 
 (* Lemma unfold_fib: forall oh0 m0 vs0, *)
@@ -413,6 +437,102 @@ Abort.
 
 
 
+Lemma to_nat_opt_id: forall n, to_nat_opt (of_nat n) = Some n.
+Proof.
+  i. unfold to_nat_opt, of_nat. des_ifs.
+  - rewrite Int.signed_repr; cycle 1.
+    { generalize Int.min_signed_neg; i. split; try xomega.
+      abstr (Z.of_nat n) x.
+      admit "Need more condition".
+    }
+    rewrite Nat2Z.id. ss.
+  - admit "Need more condition".
+Abort.
+
+
+
+
+Theorem lemma
+        m vs oh
+  :
+    rel_trans (interp_program prog (ICall Fib0._fib oh m vs))
+              (interp_program FibS0.prog (ICall Fib0._fib oh m vs))
+.
+Proof.
+  destruct (parse_arg oh m vs) eqn:T; cycle 1.
+  { unfold interp_program, mrec. irw. des_ifs.
+    unfold f_fib at 2. unfold FibS0.f_fib at 2.
+    unfold unwrapU. des_ifs. irw. eapply rtc_once; left. pfold; econs; et.
+  }
+  clear_tac.
+
+  move n at top. revert_until n.
+  pattern n. eapply well_founded_ind.
+  { eapply lt_wf. }
+  clear n. intros.
+  rename x into n.
+
+  destruct n.
+  { unfold parse_arg in *. des_ifs.
+    unfold interp_program, mrec. ss. des_ifs. cbn in Heq, Heq0. des_ifs. clear_tac.
+    rewrite ! unfold_interp_mrec.
+    unfold f_fib, FibS0.f_fib. ss. rewrite T. ss. irw. refl. }
+  destruct n.
+  { unfold parse_arg in *. des_ifs.
+    unfold interp_program, mrec. ss. des_ifs. cbn in Heq, Heq0. des_ifs. clear_tac.
+    rewrite ! unfold_interp_mrec.
+    unfold f_fib, FibS0.f_fib. ss. rewrite T. ss. irw. refl. }
+
+  unfold parse_arg in T. des_ifs.
+  unfold interp_program, mrec. ss. des_ifs. cbn in Heq, Heq0. des_ifs. clear_tac.
+  rewrite unfold_fib3. unfold unwrapU, parse_arg. rewrite T. ss. rewrite bind_ret_l.
+  unfold guarantee. destruct (to_nat_opt (of_nat n)) eqn:U; cycle 1.
+  { des_ifs_safe. unfold triggerNB. irw. eapply rtc_once; left. pfold. econs; et. }
+  ss. des_ifs_safe. rewrite bind_ret_l.
+  assert(n = n0).
+  { unfold to_nat_opt, of_nat in U. des_ifs. rewrite Int.signed_repr.
+    - rewrite Nat2Z.id. ss.
+    - admit "more integer reasoning from below facts needed".
+  }
+  subst.
+  eapply rtc_n1; cycle 1.
+  { right. rewrite ! tau_eutt. refl. }
+  fold rel_trans.
+
+
+  unfold f_fib. unfold unwrapU. rewrite unfold_interp_mrec. ss. des_ifs_safe. rewrite ! bind_ret_l. ss.
+  rewrite <- H; cycle 1.
+  { instantiate (1:= n). eauto. }
+  { ss. }
+  unfold f_fib. unfold unwrapU. rewrite unfold_interp_mrec. ss. des_ifs. repeat (rewrite bind_ret_l; ss).
+  rewrite <- H; cycle 1.
+  { instantiate (1:= S n). eauto. }
+  { ss. }
+  unfold f_fib. ss. unfold unwrapU. des_ifs. ss. rewrite bind_ret_l.
+  rewrite unfold_interp_mrec. ss. rewrite bind_ret_l.
+  refl.
+
+
+
+
+
+  unfold f_fib. unfold unwrapU. des_ifs.rewrite unfold_interp_mrec. ss. des_ifs; cycle 1.
+  { irw. eapply rtc_once. left. unfold triggerNB. irw. pfold. econs; et. }
+  rewrite ! bind_ret_l. ss.
+  rewrite <- H; cycle 1.
+  { instantiate (1:= n). eauto. }
+  { ss. }
+  unfold f_fib. unfold unwrapU. rewrite unfold_interp_mrec. ss. des_ifs. repeat (rewrite bind_ret_l; ss).
+  rewrite <- H; cycle 1.
+  { instantiate (1:= S n). eauto. }
+  { ss. }
+  unfold f_fib. ss. unfold unwrapU. des_ifs. ss. rewrite bind_ret_l.
+  rewrite unfold_interp_mrec. ss. rewrite bind_ret_l.
+  refl.
+  econs.
+  pfold.
+Qed.
+
 Theorem correct: rusc defaultR [FibS1.module: Mod.t] [FibS0.module: Mod.t].
 Proof.
   eapply SimSIR.correct; ss.
@@ -428,6 +548,59 @@ Proof.
     unfold unwrapU. des_ifs. irw. pfold; econs; et.
   }
   clear_tac.
+
+  move n at top. revert_until n.
+  pattern n. eapply well_founded_ind.
+  { eapply lt_wf. }
+  clear n. intros.
+  rename x into n.
+
+  destruct n.
+  { unfold parse_arg in *. des_ifs.
+    unfold interp_program, mrec. ss. des_ifs. cbn in Heq, Heq0. des_ifs. clear_tac.
+    rewrite ! unfold_interp_mrec.
+    unfold f_fib, FibS0.f_fib. ss. rewrite T. ss. irw. pfold. econs; et. }
+  destruct n.
+  { unfold parse_arg in *. des_ifs.
+    unfold interp_program, mrec. ss. des_ifs. cbn in Heq, Heq0. des_ifs. clear_tac.
+    rewrite ! unfold_interp_mrec.
+    unfold f_fib, FibS0.f_fib. ss. rewrite T. ss. irw. pfold. econs; et. }
+
+  unfold parse_arg in T. des_ifs.
+  unfold interp_program, mrec. ss. des_ifs. cbn in Heq, Heq0. des_ifs. clear_tac.
+  rewrite unfold_fib3. unfold unwrapU, parse_arg. rewrite T. ss. rewrite bind_ret_l.
+  unfold guarantee. destruct (to_nat_opt (of_nat n)) eqn:U; cycle 1.
+  { des_ifs_safe. unfold triggerNB. irw. pfold. econs; et. }
+  ss. des_ifs_safe. rewrite bind_ret_l.
+  assert(n = n0).
+  { unfold to_nat_opt, of_nat in U. des_ifs. rewrite Int.signed_repr.
+    - rewrite Nat2Z.id. ss.
+    - admit "more integer reasoning from below facts needed".
+  }
+  subst. irw. pfold.
+
+  rewrite U.
+  assert(U: to_nat_opt (of_nat n) = Some n).
+  { admit "should be derived from guarantee". }
+  unfoldr guarantee. rewrite U. ss. des_ifs_safe. rewrite bind_ret_l.
+  assert(V: to_nat_opt (of_nat (S n)) = Some (S n)).
+  { admit "should be derived from guarantee". }
+  des_ifs. unfold guarantee. rewrite V. ss. des_ifs.
+  setoid_rewrite <- H; cycle 1.
+  { instantiate (1:= n). eauto. }
+  { ss. }
+  unfold f_fib. unfold unwrapU. rewrite unfold_interp_mrec. ss. des_ifs.
+  rewrite ! bind_ret_l. ss.
+  rewrite unfold_interp_mrec. ss. rewrite ! bind_ret_l.
+  rewrite <- H; cycle 1.
+  { instantiate (1:= S n). eauto. }
+  { ss. }
+  unfold f_fib. ss. rewrite V. ss. rewrite bind_ret_l.
+  rewrite unfold_interp_mrec. ss. rewrite bind_ret_l. refl.
+
+
+  { irw. unfold interp_program, mrec. irw. des_ifs. unfold f_fib, FibS0.f_fib. irw.
+
   ginduction n; ii; ss.
   { unfold parse_arg in *. des_ifs. irw.
     unfold interp_program, mrec. irw. des_ifs.
